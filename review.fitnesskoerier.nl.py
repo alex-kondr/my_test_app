87acccd89033d50902e79fa4296042509105bfc9
Oleksandr Kondratiuk <alex_kondr@outlook.com>
2023-07-07 15:58:32 +0300

    save

--- review.fitnesskoerier.nl.py
+++ review.fitnesskoerier.nl.py
@@ -21,3 +21,19 @@ def process_frontpage(data, context, session):
                 name3 = cat3.xpath('text()').string()
                 url = cat3.xpath('@href').string()
                 session.queue(Request(url), process_category, dict(cat=name1+"|"+name2+"|"+name3, url=url))
+                
+
+def process_category(data, context, session):
+    prods = data.xpath('//div[@class="item is_grid with-sec-image"]')
+    for prod in prods:
+        name = prod.xpath('.//a[@class="m-img greyed"]/@title').string()
+        url = prod.xpath('.//a[@class="m-img greyed"]/@href').string()
+        session.queue(Request(url), process_product, dict(context, url=url, name=name))
+        break#################################################################################################
+
+    # page_numbers = data.xpath('//a[@class="s-pagination__link"]//text()').strings()
+    # current_page_number = int(context['url'].split('/')[-1])
+    # next_page_number = current_page_number + 1
+    # if str(next_page_number) in page_numbers:
+    #     next_url = context['url'].replace(str(current_page_number), str(next_page_number))
+    #     session.queue(Request(next_url), process_category, dict(cat=context['cat'], url=next_url))
