cd7f27ddf45672fd8fbffe048e7a287e3af7c8a4
Oleksandr Kondratiuk <alex_kondr@outlook.com>
2023-07-11 11:49:43 +0300

    save

--- review.fitnesskoerier.nl.py
+++ review.fitnesskoerier.nl.py
@@ -40,7 +40,7 @@ def process_category(data, context, session):
 
 
 def process_product(data, context, session):
-    prod_json = simplejson.loads(data.xpath('//script[@type="application/ld+json"]//text()').string().replace('\\', ''))
+    prod_json = simplejson.loads(data.xpath('//script[@type="application/ld+json"]//text()').string().replace('\\', '').replace('&#039;', "'").replace('<br />', '').replace('&quot;', "'").replace('&amp;#128170;&amp;#127997;', '').replace('&amp; ', '& ').replace('&amp;#128077;', '').replace('&amp;#128522;', ''))
 
     product = Product()
     product.name = context['name']
@@ -80,7 +80,7 @@ def process_product(data, context, session):
 
         pros_cons = rev.get('author', {}).get('name', '').split('[')
         if len(pros_cons) > 1:
-            pros_cons = pros_cons[1].replace('&quot;', '').replace('[', '').replace(']', '').replace('{', '').replace('}', '').split(',')
+            pros_cons = pros_cons[1].replace('[', '').replace(']', '').replace('{', '').replace('}', '').split(',')
             for pro_con in pros_cons:
                 if 'plus' in pro_con:
                     review.add_property(type='pros', value=pro_con.split(':')[1])
@@ -93,7 +93,6 @@ def process_product(data, context, session):
 
         excerpt = rev.get('description')
         if excerpt:
-            excerpt = excerpt.replace(' <br />', '').replace('&#039;&#039;', '"')
             review.add_property(type='excerpt', value=excerpt)
             review.ssid = review.digest() if author else review.digest(excerpt)
             product.reviews.append(review)
