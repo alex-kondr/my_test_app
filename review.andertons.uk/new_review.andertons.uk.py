6fc3757603b32361e1249009e11e710a5ff45de4
Oleksandr Kondratiuk <alex_kondr@outlook.com>
2023-07-14 16:04:46 +0300

    save

--- review.andertons.uk/new_review.andertons.uk.py
+++ review.andertons.uk/new_review.andertons.uk.py
@@ -3,7 +3,6 @@ from models.products import *
 
 
 XCAT = ["Offers", "Brands", "Customer Service", "Corporate Info"]
-PAGE = '?pageNumber='
 
 
 def run(context, session):
@@ -12,35 +11,35 @@ def run(context, session):
 
 
 def process_frontpage(data, context, session):
-    page_num = '1'
-    cats1 = data.xpath("//div[@class='dtb-sitemap__segment-content']")
-    for cat1 in cats1:
-        cat1_name = cat1.xpath('.//a[@aria-level="2"]/text()').string()
-        if cat1_name in XCAT:
-            continue
+    cats = data.xpath("//div[@class='dtb-sitemap__segment-content']")
+    for cat in cats:
+        name = cat.xpath('.//a[@aria-level="2"]/text()').string()
+        url = cat.xpath('.//a[@aria-level="2"]/@href').string()
         
-        cats2 = cat1.xpath('.//li[@class="h2 dtb-sitemap__segment-subtitle"]')
-        if cats2:
-            for cat2 in cats2:
-                cat2_name = cat2.xpath('a[@aria-level="3"]/text()').string()
-                
-                cats3 = cat2.xpath('.//li[@class="h3"]/a')
-                if cats3:
-                    for cat3 in cats3:
-                        cat3_name = cat3.xpath("text()").string()
-                        url = cat3.xpath("@href").string()
-                        if url:
-                            session.queue(Request(url + PAGE + page_num), process_category, dict(cat=cat1_name+'|'+cat2_name+'|'+cat3_name, page_num=page_num))
-                else:
-                    url = cat2.xpath('a[@aria-level="3"]/@href').string()
-                    if url:
-                        session.queue(Request(url + PAGE + page_num), process_category, dict(cat=cat1_name+'|'+cat2_name, page_num=page_num))
-                
-        else:
-            url = cat1.xpath('.//a[@aria-level="2"]/@href').string()
-            if url:
-                session.queue(Request(url + PAGE + page_num), process_category, dict(cat=cat1_name, page_num=page_num))
-        
+        if name not in XCAT:        
+            cats1 = cat.xpath('.//li[@class="h2 dtb-sitemap__segment-subtitle"]')
+            
+            if cats1:
+                for cat1 in cats1:
+                    cat1_name = cat1.xpath('a[@aria-level="3"]/text()').string()
+                    if name != cat1_name:
+                        cat1_name = name + '|' + cat1_name
+                        
+                    url = cat1.xpath('a[@aria-level="3"]/@href').string()
+                    
+                    subcats = cat1.xpath('.//li[@class="h3"]/a')
+                    if subcats:
+                        for subcat in subcats:
+                            subcat_name = subcat.xpath("text()").string()
+                            url = subcat.xpath("@href").string()
+                            if url:
+                                session.queue(Request(url+'?pageNumber=1'), process_category, dict(cat=cat1_name+'|'+subcat_name))
+                    elif url:
+                        session.queue(Request(url+'?pageNumber=1'), process_category, dict(cat=cat1_name))
+                    
+            elif url:
+                session.queue(Request(url+'?pageNumber=1'), process_category, dict(cat=name))
+
 
 def process_category(data, context, session):
     prods = data.xpath('//div[@class="c-product-grid"]//div[@class="o-tile"]')
@@ -52,23 +51,30 @@ def process_category(data, context, session):
         if revs:
             session.queue(Request(url), process_product, dict(context, name=name, url=url))
             
-    prod_count = data.xpath('//div[@class="flex-groww"]/p/text()').string()
-    if not prod_count:
+    prods_cnt = data.xpath('//div[@class="flex-groww"]/p/text()').string()
+    if not prods_cnt:
         return
-    prod_count = prod_count.split(' - ')[1].split(' of ')
-    if prod_count[0] != prod_count[1]:
-        next_page_num = str(int(context['page_num']) + 1)
-        session.queue(Request(data.response_url.replace(PAGE+context['page_num'], PAGE+next_page_num)), process_category, dict(context, page_num=next_page_num))
-        
-        
+    
+    prods_cnt = prods_cnt.split(' - ')[1].split(' of ')
+    offset = context.get('offset', 0) + len(prods)
+    if offset < prods_cnt:
+        curent_page = context.get('page', 1)
+        next_page = curent_page + 1
+        next_url = data.response_url.replace('?pageNumber=' + str(curent_page), '?pageNumber=' + str(next_page))
+        session.queue(Request(next_url), process_category, dict(context, offset=offset, page=next_page))
+
+
 def process_product(data, context, session):
     product = Product()
     product.name = context['name']
     product.url = context['url']
     product.category = context['cat']
     product.ssid = product.url.split('/')[-1]
-    product.sku = product.ssid
-    product.add_property(type='id.manufacturer', value=product.ssid)
+    
+    mpn = data.xpath('//p[@class="o-part-number" and contains(text(), "SKU:")]/text()').string()
+    if mpn:
+        mpn = mpn.replace('SKU:', '').strip()
+        product.add_property(type='id.manufacturer', value=mpn)
 
     revs = data.xpath('//div[@class="o-customer-review"]')
     for rev in revs:
