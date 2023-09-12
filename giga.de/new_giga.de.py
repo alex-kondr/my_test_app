86d2e5233392cbe77ef910ea9306485c929966e9
Oleksandr Kondratiuk <alex_kondr@outlook.com>
2023-09-12 16:58:55 +0300

    save

--- giga.de/new_giga.de.py
+++ giga.de/new_giga.de.py
@@ -1,8 +1,9 @@
 from agent import *
 from models.products import *
 
-
+# URL = 'https://www.giga.de/laptops/dell-xps-15-2-in-1/tests/dell-xps-15-2-in-1-9575-im-test-4k-notebook-mit-intel-amd-kombiprozessor/'
 def run(context, session):
+    # session.queue(Request(URL), process_review, dict(url=URL, title='title'))
     session.queue(Request('https://www.giga.de/tech/tests/'), process_revlist, dict())
 
 
@@ -20,7 +21,7 @@ def process_revlist(data, context, session):
 
 def process_review(data, context, session):
     product = Product()
-    product.name = context['title'].split(' im Test')[0].split('im Alltagstest')[0].split(' von ')[0].split(':')[0].strip()
+    product.name = context['title'].split(' im Test')[0].split('im Alltagstest')[0].split(' von ')[0].split(':')[0].split(' im ')[0].strip()
     product.ssid = context['url'].split('/')[-2]
     product.url = context['url']
 
@@ -45,21 +46,6 @@ def process_review(data, context, session):
         author_url = author.xpath("@href").string()
         review.authors.append(Person(name=author_name, profile_url=author_url, ssid=author_name))
 
-# https://www.giga.de/audio/beoplay-h4/tests/beoplay-h4-im-test-einstieg-ins-kabellose-luxussegment/
-    grades = data.xpath('//p[contains(., "Einzelwertung")]/following-sibling::ul[1]/li')
-    if not grades:
-        grades = data.xpath('//h3[contains(., "Wertung im Detail")]/following-sibling::dl/dt')
-
-    for grade in grades:
-        if ':' in grade:
-            name, grade = grade.xpath('text()').string().split(':')
-            grade = grade.split()[0].strip()
-            if '/' in grade:
-                grade, best = grade.split('/')
-                review.grades.append(Grade(name=name, value=float(grade)), best=float(best))
-            else:
-                review.grades.append(Grade(name=name, value=float(grade)), best=100.0)
-
 # https://www.giga.de/test/oppo-watch-im-test-endlich-eine-apple-watch-fuer-android-nutzer/
     grade_overall = data.xpath("//div[@class='product-rating-rating']/strong//text()").string()
     if grade_overall:
@@ -67,30 +53,77 @@ def process_review(data, context, session):
         review.grades.append(Grade(type='overall', value=float(grade_overall), best=10.0))
 
     if not grade_overall:
+        grade_overall = data.xpath('//p[strong[contains(., "Gesamt:")]]//text()').string(multiple=True)
+    if not grade_overall:
         grade_overall = data.xpath('//strong[contains(., "Gesamt:")]//text()').string(multiple=True)
     if not grade_overall:
         grade_overall = data.xpath('//p[contains(., "Gesamt:")]//text()').string(multiple=True)
-        if grade_overall:
-            grade_overall = grade_overall.split('Prozent')[0].replace('Gesamt:', '').replace('Gesamt :', '').replace('(gerundet)', '').replace('%', '').replace(',', '.').strip()
-            best = 100.0
-            if '/' in grade_overall:
-                grade_overall, best = grade_overall.split('/')
+    if not grade_overall:
+        grade_overall = data.xpath('//p[strong[contains(., "Wertung:") or contains(., "Gesamtwertung:")]]//text()').string(multiple=True)
+    if not grade_overall:
+        grade_overall = data.xpath('//strong[contains(., "Wertung:")]//text()').string(multiple=True)
 
-            review.grades.append(Grade(type='overall', value=float(grade_overall), best=float(best)))
+    if grade_overall and not review.grades:
+        grade_overall = grade_overall.split('Prozent')[0].split(':')[-1].replace('Gesamt:', '').replace('Gesamt :', '').replace('(gerundet)', '').replace('%', '').replace(',', '.').strip()
+        best = 100
+        if '/' in grade_overall:
+            grade_overall, best = grade_overall.split('/')
+            best = best.split()[0]
+
+        review.grades.append(Grade(type='overall', value=float(grade_overall), best=float(best)))
+
+# https://www.giga.de/audio/beoplay-h4/tests/beoplay-h4-im-test-einstieg-ins-kabellose-luxussegment/
+# https://www.giga.de/laptops/dell-xps-15-2-in-1/tests/dell-xps-15-2-in-1-9575-im-test-4k-notebook-mit-intel-amd-kombiprozessor/
+    grades = data.xpath('//p[contains(., "Einzelwertung")]/following-sibling::ul[1]/li')
+    if not grades:
+        grades = data.xpath('//h3[contains(., "Wertung im Detail")]/following-sibling::dl/dt')
+    if not grades:
+        grades = data.xpath('//h2[contains(., "Bewertung")]/following-sibling::ul[1]/li')
+
+    for grade in grades:
+        grade = grade.xpath('text()').string()
+        if ':' in grade:
+            name, grade = grade.split(':')
+            grade = grade.split()[0].strip()
+            if '/' in grade:
+                grade, best = grade.split('/')
+                review.grades.append(Grade(name=name, value=float(grade), best=float(best)))
+            else:
+                review.grades.append(Grade(name=name, value=float(grade), best=100.0))
+
+    if not grades:
+        names = data.xpath('//h2[contains(., "Wertung")]/following-sibling::p[1]/strong/text()').strings()
+        grades = data.xpath('//h2[contains(., "Wertung")]/following-sibling::p[1]/text()').strings()
+        if names and grades:
+            names = [name.replace(':', '').strip() for name in names]
+            for i, grade in enumerate(grades):
+                grade, best = grade.split('/')
+                review.grades.append(Grade(name=names[i], value=float(grade), best=float(best)))
+
 
     pros = data.xpath('//p[contains(., "Vorteile")]/following-sibling::ul[1]/li')
     if not pros:
+        pros = data.xpath('//h3[contains(., "Vorteile")]/following-sibling::ul[1]/li')
+    if not pros:
         pros = data.xpath('//strong[text()="Pro"]/following-sibling::ul/li')
+    if not pros:
+        pros = data.xpath('//p[strong[text()="Pro:"]]/following-sibling::ul[1]/li')
+
     for pro in pros:
         pro = pro.xpath('text').string()
         review.add_property(type='pros', value=pro)
 
-    cons = data.xpath('//p[contains(., "Nachteile")]/following-sibling::ul[1]/li')
+    cons = data.xpath('//p[strong[contains(., "Nachteile")]]/following-sibling::ul[1]/li[not(contains(., "/"))]')
+    if not cons:
+        cons = data.xpath('//h3[contains(., "Nachteile")]/following-sibling::ul[1]/li[not(contains(., "/"))]')
     if not cons:
         cons = data.xpath('//strong[text()="Kontra"]/following-sibling::ul/li')
+    if not cons:
+        cons = data.xpath('//p[strong[text()="Contra:"]]/following-sibling::ul[1]/li')
+
     for con in cons:
         con = con.xpath('text()').string()
-        review.add_property(type='pros', value=con)
+        review.add_property(type='cons', value=con)
 
     summary = data.xpath('//div[@data-init="toc-box"]/preceding-sibling::p/text()').string()
     if not summary:
@@ -99,6 +132,7 @@ def process_review(data, context, session):
         summary = data.xpath('//div[@class="product-box-content"]/following-sibling::p[1]//text()').string()
     if not summary:
         summary = data.xpath('//div[contains(@class, "alice-layout-article-body")]/p//text()').string()
+
     if summary:
         review.add_property(type='summary', value=summary)
 
@@ -106,7 +140,7 @@ def process_review(data, context, session):
     if not conclusion:
         conclusion = data.xpath('//h2[contains(., "Fazit:")]/following-sibling::p[not(contains(., "Vorteile") or contains(., "Nachteile") or contains(., "Gesamt:") or contains(., "Einzelwertung") or contains(., "Facebook") or contains(., "Twitter") or figure)]//text()').string(multiple=True)
     if not conclusion:
-        conclusion = data.xpath('//div[@class="topic"]/p[not(contains(., "Vorteile:") or contains(., "Nachteile:") or contains(., "Gesamt") or contains(., "Facebook") or contains(., "Twitter"))]//text()').string(multiple=True)
+        conclusion = data.xpath('//div[@class="topic"]/p[not(contains(., "Wertung:") or contains(., "Pro:") or contains(., "Contra:") or contains(., "Gut zu wissen:") or contains(., "Vorteile:") or contains(., "Nachteile:") or contains(., "Gesamt") or contains(., "Facebook") or contains(., "Twitter"))]//text()').string(multiple=True)
     if not conclusion:
         conclusion = data.xpath('(//p[contains(., "Fazit:")]|//p[contains(., "Fazit:")]/following-sibling::p[not(contains(., "Facebook") or contains(., "Twitter"))])//text()').string(multiple=True)
     if not conclusion:
