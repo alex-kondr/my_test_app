440a4bb3c1b756cc9f7a7d5cfed6bad72e784228
Oleksandr Kondratiuk <alex_kondr@outlook.com>
2023-10-06 15:37:58 +0300

    save

--- reviews.coffeefriend.se/reviews.coffeefriend.se.py
+++ reviews.coffeefriend.se/reviews.coffeefriend.se.py
@@ -22,29 +22,27 @@ def process_frontpage(data, context, session):
                 sub_cats1 = sub_cat.xpath('ul/li')
                 for sub_cat1 in sub_cats1:
                     sub_cat1_name = sub_cat1.xpath('a/text()').string()
-                    if not sub_cat1_name.startswith('Alla'):
+                    if not sub_cat1_name.startswith('All'):
                         url = sub_cat1.xpath('a/@href').string()
                         session.queue(Request(url+'?orderby=rating', use='curl', options='--data-raw "ppp=48"', max_age=0), process_prodlist, dict(cat=cat_name+'|'+sub_cat_name+'|'+sub_cat1_name))
 
 
 def process_prodlist(data, context, session):
     prods = data.xpath('//div[@class="row"]/ul/li')
-    count_prods = data.xpath('count(//div[@class="row"]/ul/li)')
-    count_prods_revs = 0
+
     for prod in prods:
         revs_count = prod.xpath('.//span[@itemprop="reviewCount"]/text()').string()
-        if not revs_count or int(revs_count) == 0:
-            break
-
-        count_prods_revs += 1
-        name = prod.xpath('div/@data-name').string()
-        sku = prod.xpath('div/@data-sku').string()
-        id = prod.xpath('div/@data-id').string()
-        url = prod.xpath('.//a[@itemprop="url"]/@href').string()
-        session.queue(Request(url), process_product, dict(context, name=name, sku=sku, id=id, url=url))
+        if revs_count and int(revs_count) > 0:
+            name = prod.xpath('div/@data-name').string()
+            sku = prod.xpath('div/@data-sku').string()
+            ssid = prod.xpath('div/@data-id').string()
+            url = prod.xpath('.//a[@itemprop="url"]/@href').string()
+            session.queue(Request(url), process_product, dict(context, name=name, sku=sku, ssid=ssid, url=url))
+        else:
+            return
 
     next_url = data.xpath('//a[contains(@class, "next")]/@href').string()
-    if count_prods == count_prods_revs and next_url:
+    if next_url:
         session.queue(Request(next_url+'?orderby=rating', use='curl', options='--data-raw "ppp=48"', max_age=0), process_prodlist, dict(context))
 
 
@@ -52,11 +50,10 @@ def process_product(data, context, session):
     product = Product()
     product.name = context['name']
     product.url = context['url']
-    product.category = context['cat']
-    product.ssid = context['url'].split('/')[-2]
+    product.category = context['cat'].replace('Typ|', '')
+    product.ssid = context['ssid']
     product.sku = context['sku']
     product.manufacturer = data.xpath('//div[@class="spec" and contains(., "Tillverkare")]/div[not(text()="Tillverkare")]/text()').string()
-    product.add_property(type='id.manufacturer', value=context['id'])
 
     ean = data.xpath('//div[@class="spec" and contains(., "EAN")]/div[not(text()="EAN")]/text()').string()
     if ean:
@@ -70,9 +67,11 @@ def process_product(data, context, session):
         review.date = rev.xpath('./following-sibling::body[1]/div[@class="comment-date"]/text()').string()
 
         helpful = rev.xpath('./following-sibling::body[1]//span[@class="useful"]/text()').string()
-        not_helpful = rev.xpath('./following-sibling::body[1]//span[@class="not-useful"]/text()').string()
-        if (helpful or not_helpful) and (int(helpful) > 0 or int(not_helpful) > 0):
+        if helpful and int(helpful) > 0:
             review.add_property(type='helpful_votes', value=int(helpful))
+
+        not_helpful = rev.xpath('./following-sibling::body[1]//span[@class="not-useful"]/text()').string()
+        if not_helpful and int(not_helpful) > 0:
             review.add_property(type='not_helpful_votes', value=int(not_helpful))
 
         grade_overall = rev.xpath('./following-sibling::head[1]/meta[@itemprop="ratingValue"]/@content').string()
