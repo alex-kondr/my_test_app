e5d33eba1a2f944d165612cb12dcad6205f53522
Oleksandr Kondratiuk <alex_kondr@outlook.com>
2024-03-21 17:55:35 +0200

    save

--- reviews.bagsonline.de/new_reviews.bagsonline.de.py
+++ reviews.bagsonline.de/new_reviews.bagsonline.de.py
@@ -1,5 +1,30 @@
 from agent import *
 from models.products import *
+import re
+
+
+def remove_emoji(string):
+    emoji_pattern = re.compile("["
+                               u"\U0001F600-\U0001F64F"  # emoticons
+                               u"\U0001F300-\U0001F5FF"  # symbols & pictographs
+                               u"\U0001F680-\U0001F6FF"  # transport & map symbols
+                               u"\U0001F1E0-\U0001F1FF"  # flags (iOS)
+                               u"\U00002500-\U00002BEF"  # chinese char
+                               u"\U00002702-\U000027B0"
+                               u"\U00002702-\U000027B0"
+                               u"\U000024C2-\U0001F251"
+                               u"\U0001f926-\U0001f937"
+                               u"\U00010000-\U0010ffff"
+                               u"\u2640-\u2642"
+                               u"\u2600-\u2B55"
+                               u"\u200d"
+                               u"\u23cf"
+                               u"\u23e9"
+                               u"\u231a"
+                               u"\ufe0f"  # dingbats
+                               u"\u3030"
+                               "]+", flags=re.UNICODE)
+    return emoji_pattern.sub(r'', string)
 
 
 def run(context, session):
@@ -36,7 +61,7 @@ def process_product(data, context, session):
     product.name = context['name']
     product.url = context['url']
     product.category = context['cat']
-    product.manufacturer = data.xpath('//div[@class="product--supplier"]//img/@alt').string()
+    product.manufacturer = data.xpath('//div[@itemprop="brand"]/meta[@itemprop="name"]/@content').string()
     product.sku = data.xpath('//div/@data-main-ordernumber').string()
     product.ssid = product.sku
 
@@ -69,12 +94,13 @@ def process_reviews(data, context, session):
 
         title = rev.xpath('.//h4[@class="content--title"]//text()').string(multiple=True)
         excerpt = rev.xpath('.//p[@itemprop="reviewBody"]//text()').string(multiple=True)
-        if excerpt:
-            review.title = title
+        if excerpt and title:
+            review.title = remove_emoji(title)
         elif title:
-            excerpt = title
+            excerpt = remove_emoji(title)
 
         if excerpt:
+            excerpt = remove_emoji(excerpt)
             review.add_property(type='excerpt', value=excerpt)
 
             review.ssid = review.digest() if author else review.digest(excerpt)
