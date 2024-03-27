7609cf5cd30ee1142aca08fd76b8a5f73cade66d
Oleksandr Kondratiuk <alex_kondr@outlook.com>
2024-03-27 11:44:37 +0200

    save

--- reviews.drhauschka.de/new_reviews.drhauschka.de.py
+++ reviews.drhauschka.de/new_reviews.drhauschka.de.py
@@ -1,13 +1,37 @@
 from agent import *
 from models.products import *
+import re
 
 
 XCAT = ['Behandlungen ', 'Beratung ', 'Hautberatungsteam', 'Werte', 'Magazin']
 
 
+def remove_emoji(string):
+    emoji_pattern = re.compile("["
+                               u"\U0001F600-\U0001F64F"  # emoticons
+                               u"\U0001F300-\U0001F5FF"  # symbols & pictographs
+                               u"\U0001F680-\U0001F6FF"  # transport & map symbols
+                               u"\U0001F1E0-\U0001F1FF"  # flags (iOS)
+                               u"\U00002500-\U00002BEF"  # chinese char
+                               u"\U00002702-\U000027B0"
+                               u"\U00002702-\U000027B0"
+                               u"\U000024C2-\U0001F251"
+                               u"\U0001f926-\U0001f937"
+                               u"\U00010000-\U0010ffff"
+                               u"\u2640-\u2642"
+                               u"\u2600-\u2B55"
+                               u"\u200d"
+                               u"\u23cf"
+                               u"\u23e9"
+                               u"\u231a"
+                               u"\ufe0f"  # dingbats
+                               u"\u3030"
+                               "]+", flags=re.UNICODE)
+    return emoji_pattern.sub(r'', string)
+
+
 def run(context, session):
     session.browser.use_new_parser = True
-    session.sessionbreakers = [SessionBreak(max_requests=10000)]
     session.queue(Request('https://www.drhauschka.de/'), process_frontpage, dict())
 
 
@@ -63,7 +87,7 @@ def process_product(data, context, session):
 
     ean = data.xpath('//meta[@itemprop="gtin13"]/@content').string()
     if ean and ean.isdigit() and len(ean) > 12:
-        product.add_property(type='id.ea', value=ean)
+        product.add_property(type='id.ean', value=ean)
 
     context['product'] = product
 
@@ -80,9 +104,9 @@ def process_reviews(data, context, session):
         review.url = product.url
         review.date = rev.xpath('.//meta[@itemprop="datePublished"]/@content').string()
 
-        author = rev.xpath('.//a[@itemprop="author"]/text()').string()
+        author = rev.xpath('.//*[@itemprop="author"]/text()').string()
         if author:
-            review.authors.append(Author(name=author, ssid=author))
+            review.authors.append(Person(name=author, ssid=author))
 
         grade_overall = rev.xpath('.//meta[@itemprop="ratingValue"]/@content').string()
         if grade_overall:
@@ -99,14 +123,15 @@ def process_reviews(data, context, session):
         title = rev.xpath('.//h4[@class="content--title"]//text()').string(multiple=True)
         excerpt = rev.xpath('.//p[@itemprop="reviewBody"]//text()').string(multiple=True)
         if excerpt:
-            review.title = title
+            review.title = remove_emoji(title)
         else:
-            excerpt = title
+            excerpt = remove_emoji(title)
 
         if excerpt:
-            review.ssid = review.digest() if author else review.digest(excerpt)
-
+            excerpt = remove_emoji(excerpt)
             review.add_property(type='excerpt', value=excerpt)
+
+            review.ssid = review.digest() if author else review.digest(excerpt)
 
             product.reviews.append(review)
 
