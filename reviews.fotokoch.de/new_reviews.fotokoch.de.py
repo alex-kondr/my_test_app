aa9e2202013a08e2760eacfad74106fde88d0ba1
Oleksandr Kondratiuk <48170652+alex-kondr@users.noreply.github.com>
2024-04-05 07:32:02 +0300

    save

--- reviews.fotokoch.de/new_reviews.fotokoch.de.py
+++ reviews.fotokoch.de/new_reviews.fotokoch.de.py
@@ -23,9 +23,14 @@ def process_frontpage(data, context, session):
                 sub_cats1 = sub_cat.xpath('a[@class="navi"]')
                 for sub_cat1 in sub_cats1:
                     sub_name1 = sub_cat1.xpath('text()').string()
+                    url = sub_cat1.xpath('@href').string()
 
                     if sub_name1 not in XCAT:
-                        url = sub_cat.xpath('@href').string()
+                        
+                        print('url', url)
+                        print('name', name)
+                        print('name1', sub_name)
+                        print('name2', sub_name1)
                         session.queue(Request(url + '?listenlimit=0,50'), process_prodlist, dict(cat=name + '|' + sub_name + '|' + sub_name1))
                 else:
                     url = sub_cat.xpath('a[@class="nav_desktop_level_2 navi"]/@href').string()
@@ -64,16 +69,52 @@ def process_product(data, context, session):
     if ean and ean.isdigit() and len(ean) == 13:
         product.add_property(type='id.ean')
 
-    context['product'] = product
-
-    process_reviews(data, context, session)
+    session.queue(Request(product.url.replace('.html', '_bewertungen.html')), process_reviews, dict(product=product))
 
 
 def process_reviews(data, context, session):
     product = context['product']
 
-    revs = data.xpath('')
+    revs = data.xpath('//div[@class="flex-container bewertung-liste"]')
     for rev in revs:
         review = Review()
         review.type = 'user'
         review.url = product.url
+
+        date = rev.xpath('.//div[@class="bewertung-user"]//span[contains(., "am")]/text()[normalize-space()]').string()
+        if date:
+            review.date = date.replace('am ', '').split()[0]
+
+        author = rev.xpath('.//div[@class="bewertung-user"]//span[contains(., "von")]/text()[normalize-space()]').string()
+        if author:
+            author = author.replace('von ', '')
+            review.authors.append(Person(name=author, ssid=author))
+
+        grade_overall = rev.xpath('.//span[@class="sterne active"]/@title').string()
+        if grade_overall:
+            grade_overall = float(grade_overall.split()[0])
+            if grade_overall > 0:
+                review.grades.append(Grade(type='overall', value=grade_overall, best=5.0))
+
+        is_recommended = rev.xpath('.//p[contains(., "Ich kann dieses Produkt weiterempfehlen!")]//text()[normalize-space()]').string()
+        if is_recommended:
+            review.add_property(type='is_recommended', value=True)
+
+        title = rev.xpath('.//h4[normalize-space()]//text()').string(multiple=True)
+        excerpt = rev.xpath('.//p[@class="bewertung-text"]//text()[normalize-space()]').string(multiple=True)
+        if excerpt:
+            review.title = title
+        else:
+            excerpt = title
+
+        if excerpt:
+            review.add_property(type='excerpt', value=excerpt)
+
+            review.ssid = review.digest() if author else review.digest(excerpt)
+
+            product.reviews.append(review)
+
+    if product.reviews:
+        session.emit(product)
+
+# no next page
