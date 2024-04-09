85184a78d08fb31a6305618a74b2201e5fe9177a
Oleksandr Kondratiuk <48170652+alex-kondr@users.noreply.github.com>
2024-04-09 07:42:37 +0300

    save

--- reviews.shop4runners.com-de/new_reviews.shop4runners.com-de.py
+++ reviews.shop4runners.com-de/new_reviews.shop4runners.com-de.py
@@ -6,6 +6,7 @@ XCAT = ['Laufen', 'WEITERE', 'NEUHEITEN', 'BESTSELLER', 'Sale', 'Bestseller']
 
 
 def run(context, session):
+    session.sessionbreakers = [SessionBreak(max_requests=10000)]
     session.queue(Request('https://shop4runners.com/'), process_frontpage, dict())
 
 
@@ -49,3 +50,73 @@ def process_prodlist(data, context, session):
         rating = prod.xpath('.//div[@class="rating-number ml-2 text-md"]/span/text()').string()
         if rating and float(rating) > 0:
             session.queue(Request(url), process_product, dict(context, name=name, url=url))
+
+    next_url = data.xpath('//link[@rel="next"]/@href').string()
+    if next_url:
+        session.queue(Request(next_url), process_prodlist, dict(context))
+
+
+def process_product(data, context, session):
+    product = Product()
+    product.name = context['name']
+    product.url = context['url']
+    product.ssid = product.url.split('-')[-1]
+    product.sku = data.xpath('//meta[@itemprop="sku"]/@coontent').string()
+    product.category = context['cat']
+    product.manufacturer = data.xpath('//meta[@itemprop="sku"]/preceding-sibling::meta[@itemprop="name"]/@content').string()
+
+    mpn = data.xpath('//meta[@itemprop="gtin"]/@content').string()
+    if mpn:
+        product.add_property(type='id.manufacturer', value=mpn)
+
+    ean = data.xpath('//section/@data-loadbee-gtin').striing()
+    if ean and ean.isdigit() and len(ean) > 12:
+        product.add_property(type='id.ean', value=ean)
+
+    context['product'] = product
+
+    process_reviews(data, context, session)
+
+
+def process_reviews(data, context, session):
+    product = context['product']
+    
+    revs_cnt = int(data.xpath('//span[@class="reviews text-base-v2 relative -top-px"]/text()').string().strip('()').split()[0])
+    if revs_cnt > data.xpath('count(//div[@itemprop="review"])'):
+        raise ValueError(">>>>>>>>>>>>>>>")
+    elif revs_cnt == data.xpath('count(//div[@itemprop="review"])'):
+        print("Good")
+
+    revs = data.xpath('//div[@itemprop="review"]')
+    for rev in revs:
+        review = Review()
+        review.type = 'user'
+        review.url = product.url
+        review.date = rev.xpath('.//time/@datetime').string()
+
+        author = rev.xpath('.//strong[@itemprop="author"]//text()').string(multiple=True)
+        if author:
+            review.authors.append(Person(name=author, ssid=author))
+
+        grade_overall = rev.xpath('count(.//div[@class="rating-summary__star"])')
+        if grade_overall:
+            review.grades(Grade(type='overall', value=grade_overall, best=5.0))
+
+        title = rev.xpath('div[@itemprop="name"]//text()').string(multiple=True)
+        excerpt = rev.xpath('.//div[@itemprop="description"]//text()').string(multiple=True)
+        if excerpt:
+            review.title = title
+        else:
+            excerpt = title
+
+        if excerpt:
+            review.add_property(type='excerpt', value=excerpt)
+
+            review.ssid = review.digest() if author else review.digest(excerpt)
+
+            product.reviews.append(review)
+
+    if product.reviews:
+        session.emit(product)
+
+# no next page
