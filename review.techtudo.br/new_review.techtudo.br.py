3cd7c95935efae98685ce13b3209b540c5668587
Oleksandr Kondratiuk <alex_kondr@outlook.com>
2024-06-18 14:24:33 +0300

    save

--- review.techtudo.br/new_review.techtudo.br.py
+++ review.techtudo.br/new_review.techtudo.br.py
@@ -9,7 +9,9 @@ def run(context, session):
 
 
 def process_revlist(data, context, session):
-    revs = simplejson.loads(data.content).get('items', [])
+    data_json = simplejson.loads(data.content)
+
+    revs = data_json.get('items', [])
     for rev in revs:
         title = rev.get('content', {}).get('title')
         prod_id = rev.get('id')
@@ -17,17 +19,17 @@ def process_revlist(data, context, session):
         url = rev.get('content', {}).get('url')
         session.queue(Request(url), process_review, dict(prod_id=prod_id, date=date, title=title, url=url))
 
-    next_page = rev.get('nextPage')
+    next_page = data_json.get('nextPage')
     if next_page:
         session.queue(Request('https://falkor-cda.bastian.globo.com/tenants/techtudo/instances/694b2dee-93a8-4065-ac90-41bca2dc88ce/posts/page/' + str(next_page)), process_revlist, dict())
 
 
 def process_review(data, context, session):
     product = Product()
-    product.title = context['title'].replace('', '')
+    product.name = context['title'].replace('Review:', '').split(':')[0].replace('Review', '').replace('em review', '').replace('em Review', '').replace('; veja review', '').replace('; confira review', '').strip()
     product.url = context['url']
     product.ssid = context['prod_id']
-    product.category = 'Tech'
+    product.category = 'Tecnologia'
 
     review = Review()
     review.type = 'pro'
@@ -45,8 +47,45 @@ def process_review(data, context, session):
     elif author:
         review.authors.append(Person(name=author, ssid=author))
 
+    grade_overall = data.xpath('//span[animate[@id="score-animate"]]/text()').string()
+    if grade_overall and grade_overall.isdigigt():
+        review.grades.append(Grade(type='overall', value=float(grade_overall), best=10.0))
+
+    grades = data.xpath('//div[@class="review__attribute"]')
+    for grade in grades:
+        grade_name = grade.xpath('.//div[@class="review__attribute-name"]//text()').string()
+        grade_val = grade.xpath('.//div[@class="review__attribute-score"]//text()').string()
+        if grade_val and grade_val.isdigit():
+            review.grades.append(Grade(name=grade_name, value=float(grade_val), best=10.0))
+
+    pros = data.xpath('//h2[contains(., "Prós")]/following::ul[@class="content-unordered-list"][1]/li')
+    for pro in pros:
+        pro = pro.xpath('.//text()').string().strip('+-;')
+        if len(pro) > 1:
+            review.add_property(type='pros', value=pro)
+
+    cons = data.xpath('//h2[contains(., "Contras")]/following::ul[@class="content-unordered-list"][1]/li')
+    for con in cons:
+        con = con.xpath('.//text()').string().strip('+-;')
+        if len(con) > 1:
+            review.add_property(type='cons', value=con)
+
     summary = data.xpath('//h2[@class="content-head__subtitle"]/text()').string()
     if summary:
         review.add_property(type='summary', value=summary)
 
-    excerpt = data.xpath('//p[contains(@class, "content-text__container") and not(contains(., "Canal do TechTudo") or contains(., "Fórum TechTudo"))]')
+    conclusion = data.xpath('//h2[contains(., "vale a pena?") or contains(., "Vale a pena?")]/following::p[contains(@class, "content-text__container") and not(contains(., "Canal do TechTudo") or contains(., "Fórum TechTudo") or contains(., "Com informações de"))]//text()').string(multiple=True)
+    if not conclusion:
+        conclusion = data.xpath('//span[@class="review__comment"]//text()').string(multiple=True)
+    if conclusion:
+        review.add_property(type='conclusion', value=conclusion)
+
+    excerpt = data.xpath('//h2[contains(., "vale a pena?") or contains(., "Vale a pena?")]/preceding::p[contains(@class, "content-text__container") and not(contains(., "Canal do TechTudo") or contains(., "Fórum TechTudo") or contains(., "Com informações de"))]//text()').string(multiple=True)
+    if not excerpt:
+        excerpt = data.xpath('//p[contains(@class, "content-text__container") and not(contains(., "Canal do TechTudo") or contains(., "Fórum TechTudo") or contains(., "Com informações de"))]').string(multiple=True)
+    if excerpt:
+        review.add_property(type='excerpt', value=excerpt)
+
+        product.reviews.append(review)
+
+        session.emit(product)
