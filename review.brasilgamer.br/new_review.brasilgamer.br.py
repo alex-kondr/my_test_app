8c9cc9927ea3addf09ea9988653586d21ce8d1d5
Oleksandr Kondratiuk <alex_kondr@outlook.com>
2024-07-01 11:53:28 +0300

    save

--- review.brasilgamer.br/new_review.brasilgamer.br.py
+++ review.brasilgamer.br/new_review.brasilgamer.br.py
@@ -3,7 +3,7 @@ from models.products import *
 
 
 def run(context, session):
-    session.sessionbreakers = [SessionBreak(max_requests=4000)]
+    session.sessionbreakers = [SessionBreak(max_requests=6000)]
     session.queue(Request('http://www.brasilgamer.com.br/archive/reviews'), process_revlist, dict())
 
 
@@ -33,7 +33,7 @@ def process_review(data, context, session):
     if category:
         product.category = '|'.join(category.replace('|', '/').split(', '))
     else:
-        product.category = 'Tecnologia'
+        product.category = 'Jogos'
 
     review = Review()
     review.type = 'pro'
@@ -46,10 +46,7 @@ def process_review(data, context, session):
         review.date = date.split('T')[0]
 
     author = data.xpath('//span[@class="author"]/a/text()').string()
-    author_url = data.xpath('//span[@class="author"]/a/@href').string()
-    if author and author_url:
-        review.authors.append(Person(name=author, ssid=author, profile_url=author_url))
-    elif author:
+    if author:
         review.authors.append(Person(name=author, ssid=author))
 
     grade_overall = data.xpath('count(//div[@class="review_rating"][1]/span[@class="star"])')
@@ -100,6 +97,28 @@ def process_review(data, context, session):
 
 def process_review_next(data, context, session):
     review = context['review']
+
+    grade_overall = data.xpath('count(//div[@class="review_rating"][1]/span[@class="star"])')
+    if grade_overall and grade_overall > 0:
+        review.grades.append(Grade(type='overall', value=grade_overall, best=5.0))
+
+    if not grade_overall:
+        grade_overall = data.xpath('//span[@class="review_rating_value"]/text()').string()
+        if not grade_overall:
+            grade_overall = data.xpath('//p[contains(., "/10")]/strong/text()').string()
+        if grade_overall:
+            grade_overall = grade_overall.split('/')[0]
+            review.grades.append(Grade(type='overall', value=float(grade_overall), best=10.0))
+
+    pros = data.xpath('//td[font[@color="#169600"]]//li')
+    for pro in pros:
+        pro = pro.xpath('.//text()').string(multiple=True)
+        review.add_property(type='pros', value=pro)
+
+    cons = data.xpath('//td[font[@color="#E10000"]]//li')
+    for con in cons:
+        con = con.xpath('.//text()').string(multiple=True)
+        review.add_property(type='cons', value=con)
 
     conclusion = data.xpath('(//p[strong[contains(., "Conclusão")]]|//h2[contains(., "Conclusão")])/following-sibling::p//text()').string(multiple=True)
     if not conclusion:
