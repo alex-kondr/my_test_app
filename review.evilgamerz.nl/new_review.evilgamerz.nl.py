8e3136dde6c6273370b0687b56ed60cf9dce90ef
Oleksandr Kondratiuk <48170652+alex-kondr@users.noreply.github.com>
2024-07-05 11:01:51 +0300

    save

--- review.evilgamerz.nl/new_review.evilgamerz.nl.py
+++ review.evilgamerz.nl/new_review.evilgamerz.nl.py
@@ -8,13 +8,11 @@ def run(context, session):
 
 
 def process_revlist(data, context, session):
-    revs = data.xpath('//article[.//h2[@class="entry-title"]]')
+    revs = data.xpath('//h2[@class="entry-title"]/a')
     for rev in revs:
-        name = rev.xpath('.//h2[@class="entry-title"]/a/text()').string()
-        grade_overall = rev.xpath('.//div[@class="overlay"]/text()').string()
-        cats = rev.xpath('.//a[contains(@href, "category") and not(contains(text(), "Review") or contains(text(), "Uitgelicht"))]/text()').strings()
-        url = rev.xpath('.//h2[@class="entry-title"]/a/@href').string()
-        session.queue(Request(url), process_review, dict(name=name, grade_overall=grade_overall, cats=cats, url=url))
+        name = rev.xpath('text()').string()
+        url = rev.xpath('@href').string()
+        session.queue(Request(url), process_review, dict(name=name, url=url))
 
     next_url = data.xpath('//link[@rel="next"]/@href').string()
     if next_url:
@@ -28,18 +26,20 @@ def process_review(data, context, session):
     product.ssid = product.url.split('/')[-2]
     product.category = 'Tech'
 
+    cats = data.xpath('//div[@class="post-cat"]//span[contains(@class, "category-button") and not(contains(., "Review") or contains(., "Uitgelicht"))]')
+    if cats:
+        product.category = '/'.join([cat.xpath('.//text()').string(multiple=True) for cat in cats])
+
     prod_info = data.xpath('//div[@class="artikel_veld" and (contains(., "Auteur:") or contains(., "Genre:") or contains(., "Ontwikkelaar:"))]//text()').string(multiple=True)
+    if prod_info:
+        genre = [info for info in prod_info.split('|') if "Genre" in info]
+        if genre and cats:
+            genre = genre[0].replace('Genre:', '').strip()
+            product.category += '|' + genre
 
-    genre = [info for info in prod_info.split('|') if "Genre" in info]
-    if genre and context['cats']:
-        genre = genre[0].replace('Genre:', '').strip()
-        product.category = '/'.join([cat.strip() for cat in context['cats']]) + '|' + genre
-    elif context['cats']:
-        product.category = '/'.join([cat.strip() for cat in context['cats']])
-
-    manufacturer = [info for info in prod_info.split('|') if "Ontwikkelaar" in info]
-    if manufacturer:
-        product.manufacturer = manufacturer[0].replace('Ontwikkelaar:', '').strip()
+        manufacturer = [info for info in prod_info.split('|') if "Ontwikkelaar" in info]
+        if manufacturer:
+            product.manufacturer = manufacturer[0].replace('Ontwikkelaar:', '').strip()
 
     review = Review()
     review.type = 'pro'
@@ -51,21 +51,25 @@ def process_review(data, context, session):
     if date:
         review.date = date.split('T')[0]
 
-    author = [info for info in prod_info.split('|') if "Auteur" in info]
-    if author:
-        author = author[0].replace('Auteur:', '').strip()
-        review.authors.append(Person(name=author, ssid=author))
+    if prod_info:
+        author = [info for info in prod_info.split('|') if "Auteur" in info]
+        if author:
+            author = author[0].replace('Auteur:', '').strip()
+            review.authors.append(Person(name=author, ssid=author))
 
-    if context['grade_overall']:
-        grade_overall = float(context['grade_overall'])
-        review.grades.append(Grade(type='overall', value=grade_overall, best=10.0))
+    grade_overall = data.xpath('//span[@itemprop="ratingValue"]/text()').string()
+    if grade_overall:
+        grade_overall = float(grade_overall.replace(',', '.'))
+        review.grades.append(Grade(type='overall', value=float(grade_overall), best=10.0))
 
     grades = data.xpath('//div[@class="artikel_veld" and not(contains(., "Auteur:") or contains(., "Genre:") or contains(., "Ontwikkelaar:"))]//text()').string(multiple=True)
     if grades:
         grades = grades.split('|')
         for grade in grades:
             grade_name, grade_val = grade.split(':')
-            review.grades.append(Grade(name=grade_name.strip(), value=float(grade_val), best=10.0))
+            if grade_val.strip():
+                grade_val = float(grade_val.replace(',', '.'))
+                review.grades.append(Grade(name=grade_name.strip(), value=grade_val, best=10.0))
 
     pros = data.xpath('//div[@class="plus_veld"]')
     for pro in pros:
