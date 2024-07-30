28f86a4e4e31e61b52f0adc6cea74590ccb8e486
Oleksandr Kondratiuk <alex_kondr@outlook.com>
2024-07-30 16:21:01 +0300

    save

--- create_agent/agent.py
+++ create_agent/agent.py
@@ -10,6 +10,7 @@ class AgentForm:
         self.funcs = {
             "frontpage": self.create_frontpage,
             "revlist": self.create_revlist,
+            "product": self.create_product,
             "review": self.create_review,
             "reviews": self.create_reviews,
         }
@@ -63,7 +64,7 @@ class AgentForm:
         name_title = input("name_title: ")
         name_title_xpath = input("name_title_xpath: ")
         url_xpath = input("url_xpath: ")
-        rev_s = input("review_reviews: ")
+        prod_rev = input("product/review: ")
         next_url_xpath = input("next_url_xpath: ")
 
         text = (
@@ -72,7 +73,7 @@ class AgentForm:
             "    for rev in revs:\n"
             f"        {name_title} = rev.xpath('{name_title_xpath}').string()\n"
             f"        url = rev.xpath('{url_xpath}.string()')\n"
-            f"        session.queue(Request(url), process_{rev_s}, dict({name_title}={name_title}, url=url))\n"
+            f"        session.queue(Request(url), process_{prod_rev}, dict({name_title}={name_title}, url=url))\n"
             f"\n    next_url = data.xpath('{next_url_xpath}').string()\n"
             "    if next_url:\n"
             "        session.queue(Request(next_url), process_revlist, dict())\n"
@@ -81,7 +82,49 @@ class AgentForm:
         with open(self.file_path, "a", encoding="utf-8") as file:
             file.write(text)
 
-        self.funcs.get(rev_s)()
+        self.funcs.get(prod_rev)()
+
+    def create_product(self):
+        manufacturer_xpath = input("manufacturer_xpath?: ")
+        sku_xpath = input("sku_xpath?: ")
+        mpn_xpath = input("mpn_xpath?: ")
+        ean_xpath = input("ean_xpath?: ")
+
+        text = (
+            "\n\ndef process_product(data, context, session):\n"
+            "    product = Product()\n"
+            "    product.name = context['name']\n"
+            "    product.url = context['url']\n"
+            "    product.ssid = product.url.split('/')[-2]\n"
+            "    product.category = context['cat']\n"
+        )
+
+        text += (
+            f"    product.manufacturer = data.xpath('{manufacturer_xpath}').string()\n"
+        ) if manufacturer_xpath else ""
+
+        text += (
+            f"    product.sku = data.xpath('{sku_xpath}').string()\n"
+        ) if sku_xpath else ""
+
+        text += (
+            f"\n    mpn = data.xpath('{mpn_xpath}').string()\n"
+            "    if mpn:\n"
+            "        product.add_property(type='id.manufacturer', value=mpn)\n"
+        ) if mpn_xpath else ""
+
+        text += (
+            f"\n    ean = data.xpath('{ean_xpath}').string()\n"
+            "    if ean:\n"
+            "        product.add_property(type='id.ean', value=ean)\n"
+        ) if ean_xpath else ""
+
+        text += (
+            "\n    context['product'] = product\n"
+            "    process_reviews(data, context, session)\n"
+        )
+
+        self.create_reviews()
 
     def create_review(self):
         date_xpath = input("date_xpath: ")
@@ -157,4 +200,85 @@ class AgentForm:
             file.write(text)
 
     def create_reviews(self):
-        pass
+        revs_xpath = input("revs_xpath: ")
+        date_xpath = input("date_xpath: ")
+        author_xpath = input("author_xpath?: ")
+        grade_overall_xpath = input("grade_overall_xpath?: ")
+        pros_xpath = input("pros_xpath?: ")
+        cons_xpath = input("cons_xpath?: ")
+        summary_xpath = input("summary_xpath?: ")
+        conclusion_xpath = input("conclusion_xpath?: ")
+        title_xpath = input("title_xpath?: ")
+        excerpt_xpath = input("excerpt_xpath: ")
+        rev_ssid_xpath = input("rev_ssid_xpath: ")
+
+        text = (
+            "\n\ndef process_reviews(data, context, session):\n"
+            "    product = context['product']\n"
+            f"\n    revs = data.xpath('{revs_xpath}')\n"
+            "    for rev in revs:\n"
+            "\n        review = Review()\n"
+            "        review.type = 'user'\n"
+        )
+
+        text += (
+            "        review.url = product.url\n"
+            f"\n        date = rev.xpath('{date_xpath}').string()\n"
+            "        if date:\n"
+            "            review.date = date.split('T')[0]\n"
+        )
+
+        text += (
+            f"\n        author = rev.xpath('{author_xpath}').string()\n"
+            "        if author:\n"
+            "            review.authors.append(Person(name=author, ssid=author))\n"
+        ) if author_xpath else ""
+
+        text += (
+            f"\n        grade_overall = rev.xpath('{grade_overall_xpath}').string()\n"
+            "        if grade_overall:\n"
+            "            review.grades.append(Grade(type='overall', value=float(grade_overall), best=10.0))\n"
+        ) if grade_overall_xpath else ""
+
+        text += (
+            f"\n        pros = rev.xpath('{pros_xpath}')\n"
+            "        for pro in pros:\n"
+            "            pro = pro.xpath('.//text()').string(multiple=True)\n"
+            "            review.add_property(type='pros', value=pro)\n"
+            f"\n        cons = rev.xpath('{cons_xpath}')\n"
+            "        for con in cons:\n"
+            "            con = con.path('.//text()').string(multiple=True)\n"
+            "            review.add_property(type='cons', value=con)\n"
+        ) if pros_xpath else ""
+
+        text += (
+            f"\n        summary = rev.xpath('{summary_xpath}').string(multiple=True)\n"
+            "        if summary:\n"
+            "            review.add_property(type='summary', value=summary)\n"
+        ) if summary_xpath else ""
+
+        text += (
+            f"\n        conclusion = rev.xpath('{conclusion_xpath}').string(multiple=True)\n"
+            "        if conclusion:\n"
+            "            review.add_property(type='conclusion', value=conclusion)\n"
+        ) if conclusion_xpath else ""
+
+        text += (
+            f"\n        title = rev.xpath('{title_xpath}').string(multiple=True)\n"
+            f"        excerpt = rev.xpath('{excerpt_xpath}').string(multiple=True)\n"
+            "        if excerpt:\n"
+            "            review.title = title\n"
+            "        else:\n"
+            "            excerpt = title\n"
+            "\n        if excerpt:\n"
+            "            review.add_property(type='excerpt', value=excerpt)\n"
+            f"\n            review.ssid = rev.xpath('{rev_ssid_xpath}').string()\n"
+            "            if not review.ssid:\n"
+            "                review.ssid = review.digest() if author else review.digest(excerpt)"
+            "\n              product.reviews.append(review)\n"
+            "\n        if product.reviews:"
+            "            session.emit(product)"
+        )
+
+        with open(self.file_path, "a", encoding="utf-8") as file:
+            file.write(text)
