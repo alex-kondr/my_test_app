0e0a0a3fbe195536d0f51ce09b62b9cfdcaabcb5
Oleksandr Kondratiuk <alex_kondr@outlook.com>
2024-10-21 15:25:12 +0300

    save

--- review.androidheadlines.com/new_review.androidheadlines.com.py
+++ review.androidheadlines.com/new_review.androidheadlines.com.py
@@ -1,5 +1,6 @@
 from agent import *
 from models.products import *
+import re
 
 
 def run(context, session):
@@ -10,20 +11,28 @@ def run(context, session):
 def process_revlist(data, context, session):
     revs = data.xpath('//a[contains(@class, "post-holder")]')
     for rev in revs:
-        title = rev.xpath('text()').string()
+        title = rev.xpath('@aria-label').string()
         url = rev.xpath('@href').string()
         session.queue(Request(url), process_review, dict(title=title, url=url))
 
-    next_url = data.xpath('//lin[@rel="next"]/@href').string()
+    next_url = data.xpath('//link[@rel="next"]/@href').string()
     if next_url:
         session.queue(Request(next_url), process_revlist, dict())
 
 
 def process_review(data, context, session):
+    if re.search("Top \d+", context['title']):
+        process_reviews(data, context, session)
+        return
+
     product = Product()
-    product.name = context['title'].split('Review: ')[0].strip()
+    product.name = context['title'].split(' Review: ')[0].strip()
     product.ssid = context['url'].split('/')[-1].replace('-review', '')
     product.category = 'Tech'
+
+    product.category = data.xpath('//a[@class="taxonomy category" and not(regexp:test(., "News|Reviews"))]//text()').string()
+    if not product.category:
+        product.category = 'Tech'
 
     product.url = data.xpath('//a[contains(@rel, "sponsored")]/@href').string()
     if not product.url:
@@ -53,7 +62,7 @@ def process_review(data, context, session):
 
     pros = data.xpath('//div[.//span[@class="h3"] and contains(., "Pros")]/ul/li')
     if not pros:
-        pros. data.xpath('//h2[contains(., "The Good")]/following-sibling::ul[1]/li')
+        pros = data.xpath('//h2[contains(., "The Good")]/following-sibling::ul[1]/li')
 
     for pro in pros:
         pro = pro.xpath('.//text()').string(multiple=True)
@@ -79,6 +88,8 @@ def process_review(data, context, session):
     if not conclusion:
         conclusion = data.xpath('//h2[regexp:test(., "verdict", "i")]/following-sibling::p//text()').string(multiple=True)
     if not conclusion:
+        conclusion = data.xpath('//h2[regexp:test(., "conclusion", "i")]/following-sibling::p//text()').string(multiple=True)
+    if not conclusion:
         conclusion = data.xpath('//div[@class="row"]//div[@class="col-12"]/p[not(@class)]//span[@property="itemListElement" and not(.//label or .//input or contains(., "Sign up to receive the latest"))]//text()').string(multiple=True)
 
     if conclusion:
@@ -92,6 +103,8 @@ def process_review(data, context, session):
     if not excerpt:
         excerpt = data.xpath('//h2[regexp:test(., "verdict", "i")]/preceding-sibling::p//text()').string(multiple=True)
     if not excerpt:
+        excerpt = data.xpath('//h2[regexp:test(., "conclusion", "i")]/preceding-sibling::p//text()').stirng(multiple=True)
+    if not excerpt:
         excerpt = data.xpath('(//div[@class="entry-content"]/p|//div[@class="entry-content"]/h2)//text()').string(multiple=True)
 
     if excerpt:
@@ -100,3 +113,51 @@ def process_review(data, context, session):
         product.reviews.append(review)
 
         session.emit(product)
+
+
+def process_reviews(data, context, session):
+    prods = data.xpath("//h1[@id='title']")
+    if not prods:
+        prods = data.xpath('//p[@id="title"] | //p[.//strong][not(contains(., "Sign up") or contains(., "Deals & More") or contains(., "Main") or contains(., "Android News"))]')
+    if not prods:
+        prods = data.xpath('//*[self::h1 or self::h2][@id="title"]')
+
+    for i, prod in enumerate(prods, start=1):
+        name = prod.xpath(".//text()").string(multiple=True)
+        if not name or "Top" in name:  # if don't page title
+            continue
+
+        product = Product()
+        product.name = name
+        product.url = context["url"]
+        product.ssid = product.name.lower().replace(' ', '_').replace('-', '').replace('—', '').replace('__', '_')
+
+        product.category = data.xpath('//a[@class="taxonomy category" and not(regexp:test(., "News|Reviews"))]//text()').string()
+        if not product.category:
+            product.category = 'Tech'
+
+        review = Review()
+        review.type = 'pro'
+        review.title = product.name
+        review.url = product.url
+        review.ssid = product.ssid
+
+        date = data.xpath('//meta[@property="article:published_time"]/@content[contains(., "T")]').string()
+        if date:
+            review.date = date.split('T')[0]
+
+        author = data.xpath('//div[@class="entry-meta-author-name"]//a/text()').string()
+        author_url = data.xpath('//div[@class="entry-meta-author-name"]//a/@href').string()
+        if author and author_url:
+            author_ssid = author_url.split('/')[-1]
+            review.authors.append(Person(name=author, ssid=author_ssid, profile_url=author_url))
+        elif author:
+            review.authors.append(Person(name=author, ssid=author))
+
+        excerpt = data.xpath('following-sibling::p[count(preceding-sibling::h1)={i} and not(.//img)]//text()'.format(i=i))
+        if excerpt:
+            review.add_property(type='excerpt', value=excerpt)
+
+            product.reviews.append(review)
+
+            session.emit(product)
