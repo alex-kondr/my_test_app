6d05ee83cbb75b87d4e48101280889dedbd7cc0d
Oleksandr Kondratiuk <alex_kondr@outlook.com>
2024-12-17 19:53:39 +0200

    save

--- review.smarthomesounds.co.uk/new_review.smarthomesounds.co.uk.py
+++ review.smarthomesounds.co.uk/new_review.smarthomesounds.co.uk.py
@@ -14,7 +14,7 @@ def run(context, session):
 def process_frontpage(data, context, session):
     cats = data.xpath('//a[regexp:test(@class, "^level-0")]')
     for cat in cats:
-        name = cat.xpath('.//text()').string()
+        name = cat.xpath('.//text()').string(multiple=True)
         url = cat.xpath('@href').string()
 
         if name not in XCAT:
@@ -30,9 +30,12 @@ def process_catlist(data, context, session):
         if ' all ' not in name.lower() and 'offers' not in name.lower() and 'deals' not in name.lower():
             session.queue(Request(url), process_prodlist, dict(cat=context['cat'] + '|' + name))
 
+    if not cats:
+        process_prodlist(data, context, session)
+
 
 def process_prodlist(data, context, session):
-    prods = data.xpath('//link[@rel="next"]/@href')
+    prods = data.xpath('//a[@class="product-item-link"]')
     for prod in prods:
         name = prod.xpath('text()').string()
         url = prod.xpath('@href').string()
@@ -42,7 +45,7 @@ def process_prodlist(data, context, session):
 
     next_url = data.xpath('//link[@rel="next"]/@href').string()
     if next_url:
-        session.queue(Request(url), process_prodlist, dict(context))
+        session.queue(Request(next_url), process_prodlist, dict(context))
 
 
 def process_product(data, context, session):
@@ -50,32 +53,38 @@ def process_product(data, context, session):
     product.name = context['name']
     product.url = context['url']
     product.ssid = product.url.split('/')[-1]
+    product.category = context['cat']
 
-    sku = data.xpath('//div[@class="product_ruk_rating_snippet"]/@data-sku').string()
-    if sku:
-        product.sku = sku
+    prod_json = data.xpath('//script[@type="application/ld+json"]/text()').string()
+    if prod_json:
+        prod_json = simplejson.loads(prod_json)
 
-        revs_type = data.xpath('//a[contains(., "Reviews")]/@href').string().split('/')[-1]
-        revs_url = 'https://api.reviews.co.uk/timeline/data?store={revs_type}&page=1&per_page=150&sku={sku}'.format(revs_type=revs_type, sku=sku)
-        session.do(Resuest(revs_url, force_charset='utf-8'), process_reviews, dict(product=product))
+        product.manufacturer = prod_json.get('brand', {}).get('name')
+
+        sku = prod_json.get('offers', {}).get('offers', [{}])[0].get('sku')
+        if sku:
+            product.sku = str(sku)
+
+        mpn = prod_json.get('mpn')
+        if mpn:
+            product.add_property(type='id.manufacturer', value=str(mpn))
+
+    revs_data = data.xpath('//div[@class="product_ruk_rating_snippet"]/@data-sku').string()
+    if revs_data:
+        revs_url = 'https://api.reviews.co.uk/timeline/data?store=smart-home-sounds&page=1&per_page=150&sku={}'.format(revs_data)
+        session.do(Request(revs_url, force_charset='utf-8'), process_reviews, dict(product=product))
 
 
 def process_reviews(data, context, session):
     product = context['product']
 
-    revs_json = simplejson.loads(data.context)
+    revs_json = simplejson.loads(data.content)
 
     revs = revs_json.get('timeline', [])
     for rev in revs:
         review = Review()
         review.type = 'user'
         review.url = product.url
-
-        ssid = rev.get('_source', {}).get('_id')
-        if ssid:
-            review.ssid = ssid.split('-')[-1]
-        else:
-            review.ssid = review.digest() if author else review.digest(excerpt)
 
         date = rev.get('_source', {}).get('date_created')
         if date:
@@ -84,7 +93,7 @@ def process_reviews(data, context, session):
         author = rev.get('_source', {}).get('author')
         author_ssid = rev.get('_source', {}).get('user_id')
         if author and author_ssid:
-            review.authors.append(Person(name=author, ssid=author_ssid))
+            review.authors.append(Person(name=author, ssid=str(author_ssid)))
         elif author:
             review.authors.append(Person(name=author, ssid=author))
 
@@ -114,9 +123,9 @@ def process_reviews(data, context, session):
             else:
                 review.ssid = review.digest() if author else review.digest(excerpt)
 
-            product.reviews.append(excerpt)
+            product.reviews.append(review)
 
-    revs_cnt = rev.get('stats', {}).get('review_count', 0)
+    revs_cnt = revs_json.get('stats', {}).get('review_count', 0)
     next_page = context.get('page', 1) + 1
     offset = context.get('offset', 0) + 150
     if offset < int(revs_cnt):
