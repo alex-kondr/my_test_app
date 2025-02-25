775ddecc13db4c6e66af379dbacc115bf3470785
Oleksandr Kondratiuk <alex_kondr@outlook.com>
2025-02-25 15:16:00 +0200

    save

--- review.popsci.com/new_review.popsci.com.py
+++ review.popsci.com/new_review.popsci.com.py
@@ -9,15 +9,13 @@ def run(context, session):
 
 def process_catlist(data, context, session):
     cats = data.xpath('//li[contains(@class, "tag-list-item")]/a')
-    if cats:
-        for cat in cats:
-            context['cat'] = context.get('cat', '') + '|' + cat.xpath('text()').string()
-            url = cat.xpath('@href').string()
-            session.queue(Request(url), process_catlist, dict(context))
-    else:
-        context['cat'] = context['cat'].strip('|')
-
+    if not cats:
         process_revlist(data, context, session)
+
+    for cat in cats:
+        name = context.get('cat', '') + '|' + cat.xpath('text()').string()
+        url = cat.xpath('@href').string()
+        session.queue(Request(url), process_catlist, dict(cat=name))
 
 
 def process_revlist(data, context, session):
@@ -40,7 +38,7 @@ def process_review(data, context, session):
     product = Product()
     product.name = context['title'].split(' review:')[0].replace('Best overall:', '').split(': ')[-1].strip()
     product.ssid = context['url'].split('/')[-2].replace('-review', '')
-    product.category = context['cat']
+    product.category = context['cat'].strip('|')
 
     product.url = data.xpath('//a[contains(@class, "product-card-link")]/@href').string()
     if not product.url:
@@ -58,8 +56,9 @@ def process_review(data, context, session):
 
     authors = data.xpath('//p[contains(@class, "item-author")]//a')
     for author in authors:
+        author_ssid = author.xpath('@href').string().split('/')[-2]
         author = author.xpath('text()').string()
-        review.authors.append(Person(name=author, ssid=author))
+        review.authors.append(Person(name=author, ssid=author_ssid))
 
     pros = data.xpath('//p[contains(., "Pros")]/following-sibling::ul[1]/li')
     for pro in pros:
@@ -77,6 +76,7 @@ def process_review(data, context, session):
 
     conclusion = data.xpath('//h2[regexp:test(., "verdict|final|should buy", "i")]/following-sibling::p[not(regexp:test(., "buy now", "i"))]//text()').string(multiple=True)
     if conclusion:
+        conclusion = conclusion.strip(' :.+-\n\t')
         review.add_property(type='conclusion', value=conclusion)
 
     excerpt = data.xpath('//h2[regexp:test(., "verdict|final|should buy", "i")]/preceding-sibling::p[not(regexp:test(., "Pros|Cons"))]//text()').string(multiple=True)
@@ -84,6 +84,7 @@ def process_review(data, context, session):
         excerpt = data.xpath('//div[@class="content-wrapper"]/p[not(regexp:test(., "Pros|Cons"))]//text()').string(multiple=True)
 
     if excerpt:
+        excerpt = excerpt.strip(' :.+-\n\t')
         review.add_property(type='excerpt', value=excerpt)
 
         product.reviews.append(review)
@@ -100,7 +101,7 @@ def process_reviews(data, context, session):
 
     for i, rev in enumerate(revs, start=1):
         product = Product()
-        product.category = context['cat']
+        product.category = context['cat'].strip('|')
 
         name = rev.xpath('a[@rel="noreferrer noopener nofollow"]/text()').string()
         if not name:
@@ -130,26 +131,33 @@ def process_reviews(data, context, session):
             author = author.xpath('text()').string()
             review.authors.append(Person(name=author, ssid=author))
 
-        pros = data.xpath('following-sibling::div[count(preceding-sibling::h3[@class="wp-block-heading" and a]) = {}]/div[h4[contains(., "Pros")]]//li'.format(i))
+        pros = rev.xpath('following-sibling::div[count(preceding-sibling::h3[@class="wp-block-heading" and a]) = {}]/div[h4[contains(., "Pros")]]//li'.format(i))
         for pro in pros:
-            pro = pro.xpath('.//text()').string(multiple=True)
-            review.add_property(type='pros', value=pro)
+            pro = pro.xpath('.//text()').string(multiple=True).strip(' \n\t.-–')
+            if len(pro) > 1:
+                review.add_property(type='pros', value=pro)
 
-        cons = data.xpath('following-sibling::div[count(preceding-sibling::h3[@class="wp-block-heading" and a]) = {}]/div[h4[contains(., "Cons")]]//li'.format(i))
+        cons = rev.xpath('following-sibling::div[count(preceding-sibling::h3[@class="wp-block-heading" and a]) = {}]/div[h4[contains(., "Cons")]]//li'.format(i))
         for con in cons:
-            con = con.xpath('.//text()').string(multiple=True)
-            review.add_property(type='cons', value=con)
+            con = con.xpath('.//text()').string(multiple=True).strip(' \n\t.-–')
+            if len(con) > 1:
+                review.add_property(type='cons', value=con)
 
         conclusion = rev.xpath('following-sibling::p[count(preceding-sibling::h3[@class="wp-block-heading" and a]) = {}][not(contains(., "Specs")) and contains(., "Why it made the cut")]/text()'.format(i)).string()
         if conclusion:
+            conclusion = conclusion.strip(' :.+-\n\t')
             review.add_property(type='conclusion', value=conclusion)
 
         excerpt = rev.xpath('p//text()').string(multiple=True)
+
+        if i == len(revs) and not excerpt:
+            excerpt = rev.xpath('following-sibling::h2[1]/preceding-sibling::p[count(preceding-sibling::h3[@class="wp-block-heading" and a]) = {}][not(contains(., "Specs") or contains(., "Why it made the cut"))]//text()'.format(i)).string(multiple=True)
+
         if not excerpt:
             excerpt = rev.xpath('following-sibling::p[count(preceding-sibling::h3[@class="wp-block-heading" and a]) = {}][not(contains(., "Specs") or contains(., "Why it made the cut"))]//text()'.format(i)).string(multiple=True)
 
         if excerpt:
-            excerpt = excerpt.replace('[Editor’s note:', '').strip()
+            excerpt = excerpt.replace('[Editor’s note:', '').strip(' :.+-\n\t')
             review.add_property(type='excerpt', value=excerpt)
 
             product.reviews.append(review)
