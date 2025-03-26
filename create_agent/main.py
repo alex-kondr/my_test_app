40c263fc88dbe334d8de89c81957e3a1cf46fbe1
Oleksandr Kondratiuk <alex_kondr@outlook.com>
2025-03-26 17:03:47 +0200

    Fix typo in charset parameter and update agent ID for Netties agent

--- create_agent/main.py
+++ create_agent/main.py
@@ -12,39 +12,39 @@ from create_agent.agent import AgentForm, ProcessRun
 
 agent = AgentForm(
     # name="reviews.fotokoch.de",
-    agent_id="19052"
+    agent_id="12306"
     )
-agent.create_run(
-    # name_agent_for_test="Fotokoch [DE]",
-    # agent_id="20182",
-    url='http://www.pointgphone.com/tests-android/',
-    next_func=ProcessRun.catlist.name,
-    new_parser=True,
-    breakers=None,
-    curl=False
-)
+# agent.create_run(
+#     # name_agent_for_test="Fotokoch [DE]",
+#     # agent_id="20182",
+#     url='https://www.netties.be/index.php?arc=Hardware&arc_start=0',
+#     next_func=ProcessRun.revlist.name,
+#     new_parser=False,
+#     breakers=None,
+#     curl=False
+# )
 # agent.create_frontpage(
 #     cats_xpath='//div[@class="categories__single"]',
 #     name_xpath='div[@class="categories__single-title"]/p/text()',
 #     url_xpath='@href'
 # )
-agent.create_revlist(
-    revs_xpath='//a[@class="post-link"]',
-    name_title="title",
-    name_title_xpath='h2/text()',
-    url_xpath='@href',
-    prod_rev="review",
-    next_url_xpath='//link[@rel="next"]/@href',
-)
-# agent.create_review(
-#     date_xpath='//meta[@property="article:published_time"]/@content',
-#     author_xpath='//a[@class="author-name"]/text()',
-#     author_url_xpath='//a[@class="author-name"]/@href',
-#     grade_overall_xpath='',
-#     pros_xpath='',
-#     cons_xpath='',
-#     summary_xpath='',
-#     conclusion_xpath='//h3[contains(., "Fazit")]/following-sibling::p//text()',
-#     excerpt_with_concl_xpath='//h3[contains(., "Fazit")]/preceding-sibling::p//text()',
-#     excerpt_xpath='//div[@class="entry"]/p//text()'
+# agent.create_revlist(
+#     revs_xpath='//li[@class="lijst_archief"]/a',
+#     name_title="title",
+#     name_title_xpath='text()',
+#     url_xpath='@href',
+#     prod_rev="review",
+#     next_url_xpath='//button[contains(., "Volgende pagina")]/@onclick',
 # )
+agent.create_review(
+    date_xpath='//p[@id="gepubliceerd"]/text()',
+    author_xpath='',
+    author_url_xpath='',
+    grade_overall_xpath='',
+    pros_xpath='',
+    cons_xpath='',
+    summary_xpath='',
+    conclusion_xpath='',
+    excerpt_with_concl_xpath='',
+    excerpt_xpath='//div[contains(@id, "tekst_prom")]/a[@class="link_tekst"]//text()'
+)
---
+++ review.netties.be/new_review.netties.be.py
@@ -0,0 +1,45 @@
+from agent import *
+from models.products import *
+
+
+def run(context, session):
+    session.queue(Request('https://www.netties.be/index.php?arc=Hardware&arc_start=0', force_charset='utf-8'), process_revlist, dict())
+
+
+def process_revlist(data, context, session):
+    revs = data.xpath('//li[@class="lijst_archief"]/a')
+    for rev in revs:
+        title = rev.xpath('text()').string()
+        url = rev.xpath('@href').string()
+        session.queue(Request(url, force_charset='utf-8'), process_review, dict(title=title, url=url))
+
+    next_url = data.xpath('//button[contains(., "Volgende pagina")]/@onclick').string()
+    if next_url:
+        next_url = next_url.replace("location.href='", "").replace("';", "")
+        session.queue(Request(next_url, force_charset='utf-8'), process_revlist, dict())
+
+
+def process_review(data, context, session):
+    product = Product()
+    product.name = context['title']
+    product.url = context['url']
+    product.ssid = product.url.split('/')[-1]
+    product.category = 'Hardware'
+
+    review = Review()
+    review.type = 'pro'
+    review.title = context['title']
+    review.url = product.url
+    review.ssid = product.ssid
+
+    date = data.xpath('//p[@id="gepubliceerd"]/text()').string()
+    if date:
+        review.date = date.split()[-1]
+
+    excerpt = data.xpath('//div[contains(@id, "tekst_prom")]/a[@class="link_tekst"]//text()').string(multiple=True)
+    if excerpt:
+        review.add_property(type='excerpt', value=excerpt)
+
+        product.reviews.append(review)
+
+        session.emit(product)
