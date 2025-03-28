21a5dddb054013d6110a880e6f3574826fa0e6f4
Oleksandr Kondratiuk <alex_kondr@outlook.com>
2025-03-28 13:10:22 +0200

    save

--- reviews.coolgadget.de/new_reviews.coolgadget.de.py
+++ reviews.coolgadget.de/new_reviews.coolgadget.de.py
@@ -2,52 +2,72 @@ from agent import *
 from models.products import *
 
 
+def strip_namespace(data):
+    tmp = data.content_file + ".tmp"
+    out = file(tmp, "w")
+    for line in file(data.content_file):
+        line = line.replace('<ns0', '<')
+        line = line.replace('ns0:', '')
+        line = line.replace(' xmlns', ' abcde=')
+        out.write(line + "\n")
+    out.close()
+    os.rename(tmp, data.content_file)
+
+
 def run(context, session):
+    session.browser.use_new_parser = True
     session.sessionbreakers = [SessionBreak(max_requests=10000)]
-    session.queue(Request('https://www.coolgadget.de/', force_charset='utf-8'), process_frontpage, dict())
+    session.queue(Request('https://www.coolgadget.de/', use='curl', force_charset='utf-8'), process_frontpage, dict())
 
 
 def process_frontpage(data,context, session):
+    strip_namespace(data)
     cats = data.xpath('//a[contains(@class, "category-link")]')
     for cat in cats:
         name = cat.xpath('text()').string(multiple=True)
         url = cat.xpath('@href').string()
-        session.queue(Request(url), process_catlist, dict(cat=name))
+        session.queue(Request(url, use='curl', force_charset='utf-8'), process_catlist, dict(cat=name))
 
 
 def process_catlist(data, context, session):
+    strip_namespace(data)
+
+    cats = data.xpath('//li[contains(@class, "level-1")]/a[contains(@class, "item-link")]')
+    for cat in cats:
+        name = cat.xpath('span[contains(@class, "title")]/text()').string()
+        url = cat.xpath('@href').string()
+        session.queue(Request(url, use='curl', force_charset='utf-8'), process_subcatlist, dict(cat=context['cat'] + '|' + name))
+
+
+def process_subcatlist(data, context, session):
+    strip_namespace(data)
     sub_cats = data.xpath('//li[contains(@class, "level-2")]/a[contains(@class, "item-link")]')
     for sub_cat in sub_cats:
         sub_name = sub_cat.xpath('span[contains(@class, "title")]/text()').string()
         url = sub_cat.xpath('@href').string()
-        session.queue(Request(url + '?items=100'), process_prodlist, dict(cat=context['cat'] + '|' + sub_name))
-
-    if sub_cats:
-        return
-
-    cats = data.xpath('//a[contains(@class, "item-link")]')
-    for cat in cats:
-        name = cat.xpath('span[contains(@class, "title")]/text()').string()
-        url = cat.xpath('@href').string()
-        session.queue(Request(url), process_catlist, dict(cat=context['cat'] + '|' + name))
+        session.queue(Request(url + '?items=100', use='curl', force_charset='utf-8'), process_prodlist, dict(cat=context['cat'] + '|' + sub_name))
 
 
 def process_prodlist(data, context, session):
-    prods = data.xpath('//body/div[@data-id="ALT.ItemThumbnail"]')
+    strip_namespace(data)
+
+    prods = data.xpath('//article[contains(@class, "item")]/div[@data-id]')
     for prod in prods:
         name = prod.xpath('div[contains(@class, "title")]/a/text()').string()
         url = prod.xpath('div[contains(@class, "title")]/a/@href').string()
 
         revs_cnt = prod.xpath('.//span[@class="count"]/text()').string()
         if revs_cnt and int(revs_cnt) > 0:
-            session.queue(Request(url), process_product, dict(context, name=name, url=url))
+            session.queue(Request(url, use='curl', force_charset='utf-8'), process_product, dict(context, name=name, url=url))
 
     next_url = data.xpath('//li[contains(@class, "next-page")]/a/@href').string()
     if next_url:
-        session.queue(Request(next_url), process_prodlist, dict(context))
+        session.queue(Request(next_url, use='curl', force_charset='utf-8'), process_prodlist, dict(context))
 
 
 def process_product(data, context, session):
+    strip_namespace(data)
+
     product = Product()
     product.name = context['name']
     product.url = context['url']
@@ -65,10 +85,12 @@ def process_product(data, context, session):
         product.add_property(type='id.ean', value=ean)
 
     revs_url = data.xpath('//div[contains(@class, "show-all-reviews")]/a/@href').string()
-    session.do(Reuqest(revs_url), process_reviews, dict(product=product, url=revs_url))
+    session.do(Request(revs_url, use='curl', force_charset='utf-8'), process_reviews, dict(product=product, url=revs_url))
 
 
 def process_reviews(data,context, session):
+    strip_namespace(data)
+
     product = context['product']
 
     revs = data.xpath('//div[@data-refresh-id]')
@@ -78,7 +100,7 @@ def process_reviews(data,context, session):
         review.url = context['url']
         review.ssid = rev.xpath('@data-review-id').string()
 
-        date_author = rev.spath('.//div[contains(@class, "author")]/text()').string()
+        date_author = rev.xpath('.//div[contains(@class, "author")]/text()').string(multiple=True)
         if date_author:
             review.date = date_author.rsplit(' ', 1)[0].split()[-1]
 
@@ -90,15 +112,15 @@ def process_reviews(data,context, session):
             review.add_property(type='is_verified_buyer', value=True)
 
         hlp_yes_no = rev.xpath('.//small[@class="grey"]/text()').string()
-        if hlp_yes:
-            hlp_yes = int(hlp_yes_no.split(' von ')[0])
-            hlp_no = int(hlp_yes_no.split(' von ')[1])
+        if hlp_yes_no:
+            hlp_yes = hlp_yes_no.split(' von ')[0]
+            hlp_no = hlp_yes_no.split(' von ')[1].split()[0]
 
-            if hlp_yes > 0:
-                review.add_property(type='helpful_votes', value=hlp_yes)
+            if hlp_yes and hlp_yes.isdigit() and int(hlp_yes) > 0:
+                review.add_property(type='helpful_votes', value=int(hlp_yes))
 
-            if hlp_no > 0:
-                review.add_property(type='not_helpful_votes', value=hlp_no)
+            if hlp_no and hlp_no.isdigit() and int(hlp_no) > 0:
+                review.add_property(type='not_helpful_votes', value=int(hlp_no))
 
         title = rev.xpath('.//h4[contains(@class, "item-title")]//text()').string(multiple=True)
         excerpt = rev.xpath('.//div[contains(@class, "item-comment")]//text()').string(multiple=True)
--- userreviews.clubic.fr/new_userreviews.clubic.fr.py
+++ userreviews.clubic.fr/new_userreviews.clubic.fr.py
@@ -7,7 +7,7 @@ XCAT = ['Accueil']
 
 
 def run(context, session):
-    session.sessionbreakers = [SessionBreak(max_requests=5000)]
+    session.sessionbreakers = [SessionBreak(max_requests=6000)]
     session.queue(Request('https://www.clubic.com/test-produit/', force_charset='utf-8'), process_revlist, dict())
 
 
@@ -24,11 +24,19 @@ def process_revlist(data, context, session):
 
 
 def process_review(data, context, session):
+    revs = data.xpath('//div[@class="legacy-rating-block"]')
+    rev_name = data.xpath('//div[contains(@class, "content")]/h2[contains(@class, "title") and not(regexp:test(., "Tableau|Conclusion"))]/following-sibling::div[count(preceding-sibling::h2[contains(@class, "title")])=1][contains(@class, "body")]//h4[contains(@class, "title")]/text()').string()
+    if len(revs) > 1 and rev_name:
+        process_reviews(data, context, session)
+        return
+
     product = Product()
 
     product.name = data.xpath('//div[contains(@class, "fFEfHL")]/a[@class="un-styled-linked"]/text()').string()
     if not product.name:
-        product.name = context['title'].split(':')[0].replace('Test ', '').strip()
+        product.name = data.xpath('//div[@class="legacy-rating-block"]//h3/text()').string()
+    if not product.name:
+        product.name = context['title'].replace('Test :', '').replace('Test:', '').replace('TEST :', '').replace('TEST:', '').split(':')[0].split('|')[-1].replace('(Tech Preview)', '').replace('Test ', '').replace('TEST ', '').strip()
 
     product.url = data.xpath('//a[contains(@href, "https://redirect.affilizz.com")]/@href').string()
     if not product.url:
@@ -40,16 +48,16 @@ def process_review(data, context, session):
     else:
         product.ssid = context['url'].split('/')[-1].replace('.html', '').replace('test-', '')
 
-    # PC & Gaming|Gaming|Playstation 5
-    # PC & Gaming|Souris PC|Souris gamer
     cats = data.xpath('//ul[@id="breadcrumb-list"]/li/a/text()').strings()
     if cats:
         category = ''
         for cat in cats:
-            if cat not in XCAT and not any([cat_ for cat_ in cat.split() if cat_ in category]):
+            if 'Test' not in cat and cat not in XCAT and not any([cat_ for cat_ in cat.split() if cat_ in category]):
                 category += cat + '|'
 
-        product.category = category.strip(' |')
+        category = category.strip(' |')
+        if len(category) > 1:
+            product.category = category
     else:
         product.category = 'Tech'
 
@@ -84,13 +92,15 @@ def process_review(data, context, session):
 
     pros = data.xpath('(//div[contains(., "Les plus")]/following-sibling::ul)[1]/li')
     for pro in pros:
-        pro = pro.xpath('.//text()').string(multiple=True)
-        review.add_property(type='pros', value=pro)
+        pro = pro.xpath('.//text()').string(multiple=True).strip(' -.')
+        if len(pro) > 1:
+            review.add_property(type='pros', value=pro)
 
     cons = data.xpath('(//div[contains(., "Les moins")]/following-sibling::ul)[1]/li')
     for con in cons:
-        con = con.xpath('.//text()').string(multiple=True)
-        review.add_property(type='cons', value=con)
+        con = con.xpath('.//text()').string(multiple=True).strip(' -.')
+        if len(con) > 1:
+            review.add_property(type='cons', value=con)
 
     summary = data.xpath('//div[contains(@class, "row")]/div/p/strong//text()').string(multiple=True)
     if summary:
@@ -107,3 +117,81 @@ def process_review(data, context, session):
         product.reviews.append(review)
 
         session.emit(product)
+
+
+def process_reviews(data, context, session):
+    revs = data.xpath('//div[contains(@class, "content")]/h2[contains(@class, "title") and not(regexp:test(., "Tableau|Conclusion"))]')
+    for i, rev in enumerate(revs, start=1):
+        product = Product()
+        product.name = rev.xpath('following-sibling::div[count(preceding-sibling::h2[contains(@class, "title")])={}][contains(@class, "body")]//h4[contains(@class, "title")]/text()'.format(i)).string()
+        product.ssid = product.name.lower().replace(' ', '-')
+        product.url = context['url']
+
+        cats = data.xpath('//ul[@id="breadcrumb-list"]/li/a/text()').strings()
+        if cats:
+            category = ''
+            for cat in cats:
+                if 'Test' not in cat and cat not in XCAT and not any([cat_ for cat_ in cat.split() if cat_ in category]):
+                    category += cat + '|'
+
+            category = category.strip(' |')
+            if len(category) > 1:
+                product.category = category
+        else:
+            product.category = 'Tech'
+
+        review = Review()
+        review.type = 'pro'
+        review.title = context['title']
+        review.ssid = product.ssid
+        review.url = product.url
+
+        date = data.xpath('//meta[@property="article:published_time"]/@content').string()
+        if date:
+            review.date = date.split('T')[0]
+
+        author = data.xpath('//a[contains(@href, "https://www.clubic.com/auteur/")]/text()').string()
+        author_url = data.xpath('//a[contains(@href, "https://www.clubic.com/auteur/")]/@href').string()
+        if author and author_url:
+            author_ssid = author_url.split('/')[-1].split('-')[0]
+            review.authors.append(Person(name=author, ssid=author_ssid, profile_url=author_url))
+        elif author:
+            review.authors.append(Person(name=author, ssid=author))
+
+        grades = rev.xpath('following-sibling::div[count(preceding-sibling::h2[contains(@class, "title")])={i}][contains(@class, "body")]//div[contains(@class, "rating-block__cell--details")]//p[contains(@class, "progress")]'.format(i))
+        for grade in grades:
+            grade = grade.xpath('span/text()').strings()
+            if len(grade) == 2:
+                grade_name, grade_val = grade
+                review.grades.append(Grade(name=grade_name, value=float(grade_val), best=10.0))
+
+        pros = rev.xpath('(following-sibling::div[count(preceding-sibling::h2[contains(@class, "title")])={}][contains(@class, "body")]//ul[contains(@class, "list--pros")])[1]/li'.format(i))
+        for pro in pros:
+            pro = pro.xpath('.//text()').string(multiple=True).strip(' -.')
+            if len(pro) > 1:
+                review.add_property(type='pros', value=pro)
+
+        cons = rev.xpath('(following-sibling::div[count(preceding-sibling::h2[contains(@class, "title")])={}][contains(@class, "body")]//ul[contains(@class, "list--cons")])[1]/li'.format(i))
+        for con in cons:
+            con = con.xpath('.//text()').string(multiple=True).strip(' -.')
+            if len(con) > 1:
+                review.add_property(type='cons', value=con)
+
+        summary = data.xpath('//div[contains(@class, "row")]/div/p/strong//text()').string(multiple=True)
+        if summary:
+            review.add_property(type='summary', value=summary)
+
+        conclusion = rev.xpath('following-sibling::h3[contains(., "Conclusion")]/following-sibling::text()[count(preceding-sibling::h2[contains(@class, "title")])={}]'.format(i)).string(multiple=True)
+        if conclusion:
+            review.add_property(type='conclusion', value=conclusion)
+
+        excerpt = rev.xpath('following-sibling::h3[contains(., "Conclusion")][count(preceding-sibling::h2[contains(@class, "title")])=2]/preceding-sibling::text()[count(preceding-sibling::h2[contains(@class, "title")])=2]').string(multiple=True)
+        if not excerpt:
+            excerpt = rev.xpath('following-sibling::text()[count(preceding-sibling::h2[contains(@class, "title")])={}]'.format(i)).string(multiple=True)
+
+        if excerpt:
+            review.add_property(type='excerpt', value=excerpt)
+
+            product.reviews.append(excerpt)
+
+            session.emit(product)
