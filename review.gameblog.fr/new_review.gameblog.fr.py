f63d21634cbda6a7a1d40c08f025091ed36095ca
Oleksandr Kondratiuk <alex_kondr@outlook.com>
2025-04-08 18:18:59 +0300

    save

--- review.gameblog.fr/new_review.gameblog.fr.py
+++ review.gameblog.fr/new_review.gameblog.fr.py
@@ -1,36 +1,79 @@
 from agent import *
 from models.products import *
+import simplejson
+import re
+
+
+XCAT = ['Tech', 'Tests']
+
+
+def remove_emoji(string):
+    emoji_pattern = re.compile("["
+                               u"\U0001F600-\U0001F64F"  # emoticons
+                               u"\U0001F300-\U0001F5FF"  # symbols & pictographs
+                               u"\U0001F680-\U0001F6FF"  # transport & map symbols
+                               u"\U0001F1E0-\U0001F1FF"  # flags (iOS)
+                               u"\U00002500-\U00002BEF"  # chinese char
+                               u"\U00002702-\U000027B0"
+                               u"\U00002702-\U000027B0"
+                               u"\U000024C2-\U0001F251"
+                               u"\U0001f926-\U0001f937"
+                               u"\U00010000-\U0010ffff"
+                               u"\u2640-\u2642"
+                               u"\u2600-\u2B55"
+                               u"\u200d"
+                               u"\u23cf"
+                               u"\u23e9"
+                               u"\u231a"
+                               u"\ufe0f"  # dingbats
+                               u"\u3030"
+                               "]+", flags=re.UNICODE)
+    return emoji_pattern.sub(r'', string)
 
 
 def run(context, session):
-    session.sessionbreakers = [SessionBreak(max_requests=10000)]
-    session.queue(Request('https://www.gameblog.fr/api/posts/load_more_posts/route/list_page/controller/components_controller/method/search_post_items/view_mode/full/sort_order/desc/offset/0/ppp/10/release_filter/a-venir/search_filters/Tech%2CHardware%20Tests%2Call%2Call%2C%2C%2C%2C%2C%2C%2C%2C%2C/limit/gameblog', use='curl', force_charset='utf-8'), process_frontpage, dict())
+    session.sessionbreakers = [SessionBreak(max_requests=8000)]
+    url = 'https://www.gameblog.fr/api/posts/load_more_posts/route/list_page/controller/components_controller/method/search_post_items/view_mode/full/sort_order/desc/offset/0/ppp/10/release_filter/a-venir/search_filters/Tech%2CHardware%20Tests%2Call%2Call%2C%2C%2C%2C%2C%2C%2C%2C%2C/limit/gameblog'
+    session.queue(Request(url, force_charset='utf-8'), process_revlist, {})
 
 
 def process_revlist(data, context, session):
     revs = data.xpath('//div[@class="item-content-header"]')
+    if not revs:
+        return
+
     for rev in revs:
         title = rev.xpath('h2[@class="title"]/text()').string()
         url = rev.xpath('a[contains(@class, "title")]/@href').string()
-        session.queue(Request(url, force_charset='utf-8'), process_review, dict(title=title, url=url))
+        session.queue(Request(url, use='curl', force_charset='utf-8'), process_review, dict(title=title, url=url))
 
-    next_url = data.xpath('').string()
-    if next_url:
-        session.queue(Request(next_url, force_charset='utf-8'), process_revlist, dict())
+    offset = context.get('offset', 0) + 10
+    next_url = 'https://www.gameblog.fr/api/posts/load_more_posts/route/list_page/controller/components_controller/method/search_post_items/view_mode/full/sort_order/desc/offset/{}/ppp/10/release_filter/a-venir/search_filters/Tech%2CHardware%20Tests%2Call%2Call%2C%2C%2C%2C%2C%2C%2C%2C%2C/limit/gameblog'.format(offset)
+    session.queue(Request(next_url, force_charset='utf-8'), process_revlist, dict(offset=offset))
 
 
 def process_review(data, context, session):
     product = Product()
-    product.name = context['title'].replace('', '').strip()
+    product.name = data.xpath('//h3[contains(@class, "title ")]/a/text()').string() or context['title'].split('TEST de ')[-1].split('TEST ')[-1].split(' : ', 1)[0]
     product.url = context['url']
-    product.ssid = product.url.split('/')[-2]
-    product.category = 'Tech'
+    product.ssid = product.url.split('-')[-1]
+    product.manufacturer = data.xpath('//div[@class="developers"]/span/a/text()').string()
+
+    cats = data.xpath('//div[@class="post-tags"]/a/text()').strings()
+    cats = '|'.join([cat.strip() for cat in cats if cat.strip() not in XCAT])
+
+    platforms = data.xpath('//div[@class="platforms"]/div/a/text()').strings()
+    platforms = '|'.join([platform.strip() for platform in platforms])
+
+    product.category = (cats + '/' + platforms).strip(' |/')
+    if not product.category:
+        product.category = 'Tech'
 
     review = Review()
-    review.type = 'pro'
     review.title = context['title']
-    review.url = product.url
+    review.url = context['url']
     review.ssid = product.ssid
+    review.type = 'pro'
 
     date = data.xpath('//meta[@property="article:published_time"]/@content').string()
     if date:
@@ -40,39 +83,67 @@ def process_review(data, context, session):
     author_url = data.xpath('//a[@class="name"]/@href').string()
     if author and author_url:
         author_ssid = author_url.split('/')[-1]
-        review.authors.append(Person(name=author, ssid=author_ssid, profile_url=author_url))
-    elif author:
-        review.authors.append(Person(name=author, ssid=author))
+        review.authors.append(Person(name=author, profile_url=author_url, ssid=author_ssid))
 
-    grade_overall = data.xpath('0').string()
-    if grade_overall:
+    pros = data.xpath('(//tr[contains(., "ON A AIMÉ")]/following-sibling::tr//ul)[1]/li')
+    for pro in pros:
+        pro = pro.xpath('.//text()').string(multiple=True).replace('…', '').strip(' .\n\t+-')
+        if pro:
+            review.add_property(type='pros', value=pro)
+
+    cons = data.xpath('''(//tr[contains(., "ON N'A PAS AIMÉ")]/following-sibling::tr//ul)[2]/li''')
+    for con in cons:
+        con = con.xpath('.//text()').string(multiple=True).replace('…', '').strip(' .\n\t+-')
+        if con:
+            review.add_property(type='cons', value=con)
+
+    summary = data.xpath('//div[@class="post-excerpt"]/p//text()').string(multiple=True)
+    if summary:
+        summary = remove_emoji(summary).strip()
+        review.add_property(type="summary", value=summary)
+
+    conclusion = data.xpath('//tr/td/p[normalize-space(text())]//text()').string(multiple=True)
+    if conclusion:
+        conclusion = remove_emoji(conclusion).strip()
+        review.add_property(type="conclusion", value=conclusion)
+
+    excerpt = data.xpath('//div[@class="post-content"]/p//text()').string(multiple=True)
+    if excerpt:
+        excerpt = remove_emoji(excerpt).strip()
+        review.add_property(type="excerpt", value=excerpt)
+
+        next_url = 'https://www.gameblog.fr/v5/review_summary_component?ct=content_block_8&user_presence=false&id={}&spf=load&incoming_page_type=review_page'.format(product.ssid)
+        session.do(Request(next_url, force_charset='utf-8', max_age=0), process_review_next, dict(review=review, product=product))
+
+
+def process_review_next(data, context, session):
+    review = context['review']
+
+    new_data = simplejson.loads(data.content).get('body', {}).get('content_block_8')
+    new_data = data.parse_fragment(new_data)
+
+    pros = new_data.xpath('//div[@class="good-points"]//div[@class="point"]')
+    for pro in pros:
+        pro = pro.xpath('.//text()').string(multiple=True).replace('…', '').strip(' .\n\t+-')
+        if pro:
+            review.add_property(type='pros', value=pro)
+
+    cons = new_data.xpath('//div[@class="bad-points"]//div[@class="point"]')
+    for con in cons:
+        con = con.xpath('.//text()').string(multiple=True).replace('…', '').strip(' .\n\t+-')
+        if con:
+            review.add_property(type='cons', value=con)
+
+    grade_overall = new_data.xpath('//text[@class="rating-text"]/text()').string()
+    if grade_overall and grade_overall.isdigit():
         review.grades.append(Grade(type='overall', value=float(grade_overall), best=10.0))
 
-    pros = data.xpath('0')
-    for pro in pros:
-        pro = pro.xpath('.//text()').string(multiple=True)
-        review.add_property(type='pros', value=pro)
-
-    cons = data.xpath('0')
-    for con in cons:
-        con = con.xpath('.//text()').string(multiple=True)
-        review.add_property(type='cons', value=con)
-
-    summary = data.xpath('//p[@itemprop="description about"]//text()').string(multiple=True)
-    if summary:
-        review.add_property(type='summary', value=summary)
-
-    conclusion = data.xpath('//h2[regexp:test(., "Végszó|Fnatic")]/following-sibling::p[@class="mgt0" or @class="mgt1"][not(s)]//text()').string(multiple=True)
+    conclusion = new_data.xpath('//div[@class="review-text"]//text()').string(multiple=True)
     if conclusion:
-        review.add_property(type='conclusion', value=conclusion)
+        conclusion = remove_emoji(conclusion).strip()
+        review.add_property(type="conclusion", value=conclusion)
 
-    excerpt = data.xpath('//h2[regexp:test(., "Végszó|Fnatic")]/preceding-sibling::p[@class="mgt0" or @class="mgt1"]//text()').string(multiple=True)
-    if not excerpt:
-        excerpt = data.xpath('//div[contains(@class, "content-body")]/p[@class="mgt0" or @class="mgt1"]//text()').string(multiple=True)
+    product = context['product']
+    product.reviews.append(review)
 
-    if excerpt:
-        review.add_property(type='excerpt', value=excerpt)
-
-        product.reviews.append(review)
-
-        session.emit(product)
+    session.emit(product)
