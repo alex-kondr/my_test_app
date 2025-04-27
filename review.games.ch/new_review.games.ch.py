884bf8cb3dbee0c9b4e91663d19be78b1992a65f
Oleksandr Kondratiuk <alex_kondr@outlook.com>
2025-04-27 18:58:09 +0300

    save

---
+++ review.games.ch/new_review.games.ch.py
@@ -0,0 +1,135 @@
+from agent import *
+from models.products import *
+
+
+def run(context, session):
+    session.sessionbreakers = [SessionBreak(max_requests=4000)]
+    session.queue(Request('https://www.games.ch', use='curl', force_charset='utf-8', max_age=0), process_frontpage, dict())
+
+
+def process_frontpage(data, context, session):
+    cats = data.xpath('//ul[li/a[contains(., "Alle Spiele")]]//li[not(contains(., "Alle Spiele")) and @class=""]/a')
+    for cat in cats:
+        name = cat.xpath('.//text()').string()
+        url = cat.xpath('@href').string()
+        session.queue(Request(url + "artikel.html", use='curl', force_charset='utf-8', max_age=0), process_revlist, dict(cat=name))
+
+
+def process_revlist(data, context, session):
+    revs = data.xpath('//h3/a')
+    for rev in revs:
+        title = rev.xpath('text()').string()
+        url = rev.xpath('@href').string()
+        session.queue(Request(url, use='curl', force_charset='utf-8', max_age=0), process_review, dict(context, title=title, url=url))
+
+    next_url = data.xpath('//link[@rel="next"]/@href').string()
+    if next_url:
+        session.queue(Request(next_url, use='curl', force_charset='utf-8', max_age=0), process_revlist, dict(context))
+
+
+def process_review(data, context, session):
+    if data.xpath('//a[@itemprop="articleSection" and (@href="https://www.games.ch/swissgames/" or @href="https://www.games.ch/blizzard-entertainment/")]'):
+        return
+
+    product = Product()
+    product.name = context['title'].split(' - ')[0].split(' – ')[0].split('- ')[0].split(' -')[0].replace('im Test', '').strip()
+    product.url = context['url']
+    product.ssid = product.name.lower().replace(': ', '-').replace(' :', '-').replace(':', '-').replace(' ', '-')
+    product.category = context['cat']
+
+    review = Review()
+    review.type = 'pro'
+    review.title = context['title']
+    review.url = product.url
+    review.ssid = product.ssid
+
+    date = data.xpath('//div[not(@class="meta")]/time/@datetime').string()
+    if date:
+        review.date = date.split()[0]
+
+    authors = data.xpath('//div[not(@class="meta")]/time/following-sibling::span/a')
+    for author in authors:
+        author_name = author.xpath('text()').string()
+        author_ssid = author.xpath('@href').string().split('/')[-2]
+        review.authors.append(Person(name=author_name, ssid=author_ssid))
+
+    grade_overall = data.xpath('//h3[contains(., "Bewertung")]/span/@content').string()
+    if grade_overall:
+        review.grades.append(Grade(type='overall', value=float(grade_overall), best=100.0))
+
+    grades = data.xpath('//div[@class="label"]')
+    for grade in grades:
+        grade_val, grade_name = grade.xpath('.//text()').string(multiple=True).split('%')
+        review.grades.append(Grade(name=grade_name.strip(), value=float(grade_val), best=100.0))
+
+    pros = data.xpath('//ul[@class="pro"]/li')
+    for pro in pros:
+        pro = pro.xpath('.//text()').string(multiple=True)
+        if pro and len(pro) > 1:
+            pro = pro.strip('+- ')
+            review.add_property(type='pros', value=pro)
+
+    cons = data.xpath('//ul[@class="con"]/li')
+    for con in cons:
+        con = con.xpath('.//text()').string(multiple=True)
+        if con and len(con) > 1:
+            con = con.strip('+- ')
+            review.add_property(type='cons', value=con)
+
+    conclusion = data.xpath('//h2[contains(., "Fazit")]/following-sibling::p//text()').string(multiple=True)
+    if conclusion:
+        context['conclusion'] = conclusion
+        review.add_property(type='conclusion', value=conclusion)
+
+    excerpt = data.xpath('//h2[contains(., "Fazit")]/preceding-sibling::p//text()').string(multiple=True)
+    if not excerpt:
+        excerpt = data.xpath('//div[@class="col8"]/p//text()').string(multiple=True)
+
+    context['excerpt'] = excerpt
+
+    next_url = data.xpath('//div[@id="pagination"]//li[span]/following-sibling::li/a/@href').string()
+    if next_url:
+        title = review.title + ' - Pagina 1'
+        review.add_property(type='pages', value=dict(title=title, url=review.url))
+
+        session.do(Request(next_url, use='curl', force_charset='utf-8', max_age=0), process_review_next, dict(context, product=product, review=review, url=next_url, page=2))
+
+    else:
+        context['product'] = product
+        context['review'] = review
+        process_review_next(data, context, session)
+
+
+def process_review_next(data, context, session):
+    review = context['review']
+
+    page = context.get('page', 1)
+    if page > 1:
+        title = review.title + ' - Pagina ' + str(page)
+        review.add_property(type='pages', value=dict(title=title, url=context['url']))
+
+        conclusion = data.xpath('//h2[contains(., "Fazit")]/following-sibling::p//text()').string(multiple=True)
+        if conclusion:
+            context['conclusion'] = conclusion
+            review.add_property(type='conclusion', value=conclusion)
+
+        excerpt = data.xpath('//h2[contains(., "Fazit")]/preceding-sibling::p//text()').string(multiple=True)
+        if not excerpt:
+            excerpt = data.xpath('//div[@class="col8"]/p//text()').string(multiple=True)
+        if excerpt:
+            context['excerpt'] += " " + excerpt
+
+    next_url = data.xpath('//div[@id="pagination"]//li[span]/following-sibling::li/a/@href').string()
+    if next_url:
+        session.do(Request(next_url, use='curl', force_charset='utf-8', max_age=0), process_review_next, dict(context, review=review, url=next_url, page=page + 1))
+
+    elif context['excerpt']:
+        if context.get('conclusion'):
+            context['excerpt'] = context['excerpt'].replace(context['conclusion'], '').strip()
+
+        review.add_property(type="excerpt", value=context['excerpt'])
+
+        product = context['product']
+        product.reviews.append(review)
+
+        session.emit(product)
--- review.moviefone.com/test.py
+++ review.moviefone.com/test.py
@@ -12,7 +12,7 @@ from product_test.test_logs import LogProduct, TestLogProduct
 import product_test.list_of_agents as agents
 
 
-agent = agents.{name_agent_for_test}
+agent = agents.MOVIEFONE_US
 # agent = agents.TEST
 reload = 1
 
@@ -21,7 +21,7 @@ if __name__ == "__main__":
     product = Product(agent, reload=reload)
     print(product.result)
     test = TestProduct(product)
-    test.run(xproduct_names=[], not_xproduct_name='', len_name=3, xreview_title=[], xreview_conclusion=[], xreview_excerpt=[])
+    test.run(xproduct_names=[], not_xproduct_name='Ã', len_name=3, xreview_title=[], xreview_conclusion=[], xreview_excerpt=[])
 
     log = LogProduct(agent, reload=reload)
     test_log = TestLogProduct(log)
