86db2724a5801e447ee39f32259e58e00eb76a1b
Oleksandr Kondratiuk <alex_kondr@outlook.com>
2025-05-15 18:47:18 +0300

    save

--- review.multicom.no/new_review.multicom.no.py
+++ review.multicom.no/new_review.multicom.no.py
@@ -1,6 +1,7 @@
 from agent import *
 from models.products import *
 import simplejson
+import re
 
 
 XCAT = ['Kampanjer', 'Gavekort', 'Gjenbruksbutikken', 'Outlet', 'Vis alle']
@@ -11,8 +12,32 @@ XPROD = [
 ]
 
 
+def remove_emoji(string):
+    emoji_pattern = re.compile("["
+                               u"\U0001F600-\U0001F64F"  # emoticons
+                               u"\U0001F300-\U0001F5FF"  # symbols & pictographs
+                               u"\U0001F680-\U0001F6FF"  # transport & map symbols
+                               u"\U0001F1E0-\U0001F1FF"  # flags (iOS)
+                               u"\U00002500-\U00002BEF"  # chinese char
+                               u"\U00002702-\U000027B0"
+                               u"\U00002702-\U000027B0"
+                               u"\U000024C2-\U0001F251"
+                               u"\U0001f926-\U0001f937"
+                               u"\U00010000-\U0010ffff"
+                               u"\u2640-\u2642"
+                               u"\u2600-\u2B55"
+                               u"\u200d"
+                               u"\u23cf"
+                               u"\u23e9"
+                               u"\u231a"
+                               u"\ufe0f"  # dingbats
+                               u"\u3030"
+                               "]+", flags=re.UNICODE)
+    return emoji_pattern.sub(r'', string)
+
+
 def run(context, session):
-    session.sessionbreakers = [SessionBreak(max_requests=7000)]
+    session.sessionbreakers = [SessionBreak(max_requests=10000)]
     session.queue(Request('https://www.multicom.no/', use='curl', force_charset='utf-8'), process_frontpage, {})
 
 
@@ -121,9 +146,11 @@ def process_reviews(data, context, session):
 
         excerpt = rev.get('content')
         if excerpt:
-            review.add_property(type="excerpt", value=excerpt)
+            excerpt = remove_emoji(excerpt).replace('\n', '').strip()
+            if len(excerpt) > 2:
+                review.add_property(type="excerpt", value=excerpt)
 
-            product.reviews.append(review)
+                product.reviews.append(review)
 
     offset = context.get('offset', 0) + 10
     if offset < context['revs_cnt']:
