9726402cc4ddf1b1363b062d090e82485297b44c
Oleksandr Kondratiuk <alex_kondr@outlook.com>
2025-05-28 22:24:59 +0300

    save

--- review.hintaseuranta.fi/new_review.hintaseuranta.fi.py
+++ review.hintaseuranta.fi/new_review.hintaseuranta.fi.py
@@ -1,5 +1,6 @@
 from agent import *
 from models.products import *
+import simplejson
 
 
 def strip_namespace(data):
@@ -31,7 +32,9 @@ def process_frontpage(data, context, session):
         for sub_cat in sub_cats:
             sub_name = sub_cat.xpath('text()').string()
             url = sub_cat.xpath('@href').string()
-            session.queue(Request(url, use='curl', force_charset='utf-8'), process_catlist, dict(cat=name + '|' + sub_name))
+
+            if name and sub_name:
+                session.queue(Request(url, use='curl', force_charset='utf-8'), process_catlist, dict(cat=name + '|' + sub_name))
 
 
 def process_catlist(data, context, session):
@@ -58,19 +61,32 @@ def process_catlist(data, context, session):
 def process_prodlist(data, context, session):
     strip_namespace(data)
 
+    prods_cnt = data.xpath('//span[@class="search-hits"]/text()').string()
+    cat_id = data.response_url.split('?')[0].split('/')[-1]
+    next_data = data.xpath('//div[@id="data-values"]//@data-values').string(multiple=True)
+    options = """--compressed -X POST  --data-raw 'id={cat_id}&vals={data}&view=&sort=rating+desc'""".format(data=next_data, cat_id=cat_id)
+
+    if prods_cnt and next_data:
+        session.queue(Request('https://hintaseuranta.fi/facet/filtering', options=options, use='curl', force_charset='utf-8'), process_prodlist_sort, dict(context, next_data=next_data, prods_cnt=int(prods_cnt)))
+
+
+def process_prodlist_sort(data, context, session):
+    strip_namespace(data)
+
     prods = data.xpath('//div[@class="item-data"]')
     for prod in prods:
         name = prod.xpath('h4[not(@class)]/a/text()').string()
         url = prod.xpath('h4[not(@class)]/a/@href').string()
 
-        rating = data.xapth('div[@class="item-data-rating"]')
+        rating = prod.xpath('div[@class="item-data-rating"]')
         if rating:
             session.queue(Request(url, use='curl', force_charset='utf-8'), process_product, dict(context, name=name, url=url))
+        else:
+            return
 
-    next_data = data.xpath('//div[@id="data-values"]//@data-values').string(multiple=True)
-    if next_data:
-        offset = context.get('offset', 0) + 30
-        options = """--compressed -X POST  --data-raw 'vals={data}&skip={offset}&view=&sort=%22popularity+desc%22'""".format(data=next_data, offset=offset)
+    offset = context.get('offset', 0) + 30
+    if offset < context['prods_cnt']:
+        options = """--compressed -X POST  --data-raw 'vals={data}&skip={offset}&view=&sort=rating+desc'""".format(data=context['next_data'], offset=offset)
         session.queue(Request('https://hintaseuranta.fi/facet/listmore', options=options, use='curl', force_charset='utf-8'), process_prodlist, dict(context, offset=offset))
 
 
@@ -83,4 +99,71 @@ def process_product(data, context, session):
     product.ssid = product.url.split('/')[-1]
     product.sku = product.ssid
     product.category = context['cat']
-    
+
+    prod_json = data.xpath('''//script[contains(., '"@type":"Product"')]/text()''').string()
+    if prod_json:
+        prod_json = simplejson.loads(prod_json)
+
+        ean = prod_json.get('gtin13')
+        if ean and ean.isdigit() and len(ean) > 10:
+            product.add_property(type='id.ean', value=ean)
+
+    revs = data.xpath('//table[@id="productReviews"]/tbody/tr[.//div]')
+    for rev in revs:
+        review = Review()
+        review.type = 'user'
+        review.url = product.url
+        review.ssid = rev.xpath('.//a[contains(@href, "reviewId")]/@href').string().split('=')[-1]
+
+        date = rev.xpath('.//td[h4]/text()').string(multiple=True)
+        if date:
+            review.date = date.strip('( )')
+
+        author = rev.xpath('.//td[h4]/strong/text()').string()
+        if author:
+            review.authors.append(Person(name=author, ssid=author))
+
+        grade_overall = 0.0
+        if rev.xpath('.//i[@class="stars normal width-78 height-13 pos-y-m113 pos-x-m0"]'):
+            grade_overall = 5.0
+        elif rev.xpath('.//i[@class="stars normal width-78 height-13 pos-y-m113 pos-x-m13"]'):
+            grade_overall = 4.5
+        elif rev.xpath('.//i[@class="stars normal width-78 height-13 pos-y-m113 pos-x-m26"]'):
+            grade_overall = 4.0
+        elif rev.xpath('.//i[@class="stars normal width-78 height-13 pos-y-m113 pos-x-m39"]'):
+            grade_overall = 3.5
+        elif rev.xpath('.//i[@class="stars normal width-78 height-13 pos-y-m113 pos-x-m52"]'):
+            grade_overall = 3.0
+        elif rev.xpath('.//i[@class="stars normal width-78 height-13 pos-y-m113 pos-x-m78"]'):
+            grade_overall = 2.0
+        elif rev.xpath('.//i[@class="stars normal width-78 height-13 pos-y-m113 pos-x-m104"]'):
+            grade_overall = 1.0
+
+        if grade_overall:
+            review.grades.append(Grade(type='overall', value=grade_overall, best=5.0))
+
+        is_recommended = rev.xpath('.//div[contains(., "SUOSITTELEN")]')
+        if is_recommended:
+            review.add_property(type='is_recommended', value=True)
+
+        hlp_yes = rev.xpath('.//span[contains(@id, "votes")]/text()').string()
+        if hlp_yes and int(hlp_yes) > 0:
+            review.add_property(type='helpful_votes', value=int(hlp_yes))
+
+        title = rev.xpath('.//td/h4/text()').string()
+        excerpt = rev.xpath('.//td/p//text()').string(multiple=True)
+        if excerpt:
+            review.title = title
+        else:
+            excerpt = title
+
+        if excerpt:
+            excerpt = excerpt.replace('\n', '').strip()
+            review.add_property(type='excerpt', value=excerpt)
+
+            product.reviews.append(review)
+
+    if product.reviews:
+        session.emit(product)
+
+# no next page
