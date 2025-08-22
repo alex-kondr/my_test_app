58b21a2646661a725059fd754aecfad62b38a8e0
Oleksandr Kondratiuk <alex_kondr@outlook.com>
2025-08-22 09:26:42 +0300

    save

--- review.gameforfun.com.br/new_review.gameforfun.com.br.py
+++ review.gameforfun.com.br/new_review.gameforfun.com.br.py
@@ -21,10 +21,14 @@ def process_revlist(data: Response, context: dict[str, str], session: Session):
 
 def process_review(data: Response, context: dict[str, str], session: Session):
     product = Product()
-    product.name = context['title'].replace('', '').strip()
+    product.name = context['title'].replace('Review ', '').strip()
     product.url = context['url']
-    product.ssid = product.url.split('/')[-2]
-    product.category = 'Tech'
+    product.ssid = product.url.split('/')[-2].replace('review-', '')
+    product.category = 'Games'
+
+    platforms = data.xpath('//p[contains(text(), "nos jogos de")]/text()').string()
+    if platforms:
+        product.category += '|' + platforms.split('nos jogos de')[-1].strip().replace(', ', '/').replace(' e ', '/')
 
     review = Review()
     review.type = 'pro'
@@ -32,21 +36,31 @@ def process_review(data: Response, context: dict[str, str], session: Session):
     review.url = product.url
     review.ssid = product.ssid
 
-    date = data.xpath('//meta[@property="article:published_time"]/@content|//time/@datetime').string()
+    date = data.xpath('//meta[@property="article:published_time"]/@content').string()
     if date:
         review.date = date.split('T')[0]
 
-    author = data.xpath('/text()').string()
-    author_url = data.xpath('/@href').string()
+    author = data.xpath('//h2[contains(@class, "heading-title") and contains(., "Por: ")]//text()').string()
+    author_url = data.xpath('//h2[contains(@class, "heading-title") and contains(., "Por: ")]/a/@href').string()
     if author and author_url:
+        author = author.replace('Por: ', '').strip()
         author_ssid = author_url.split('/')[-1]
         review.authors.append(Person(name=author, ssid=author_ssid, profile_url=author_url))
     elif author:
+        author = author.replace('Por: ', '').strip()
         review.authors.append(Person(name=author, ssid=author))
 
-    grade_overall = data.xpath('//text()').string()
+    grade_overall = data.xpath('//input[@class="jet-form__field hidden-field" and contains(@name, "_value")]/@value').strings()
     if grade_overall:
-        review.grades.append(Grade(type='overall', value=float(grade_overall), best=))
+        grade_overall = round(sum([float(grade) for grade in grade_overall]) / len(grade_overall))
+        review.grades.append(Grade(type='overall', value=float(grade_overall), best=100.0))
+
+    grades = data.xpath('//input[@class="jet-form__field hidden-field" and contains(@name, "_value")]')
+    for grade in grades:
+        grade_name = grade.xpath('@name').string().replace('_value', '').title()
+        grade_val = grade.xpath('@value').string()
+        review.grades.append(Grade(name=grade_name, value=float(grade_val), best=100.0))
+
 
     pros = data.xpath('(//h3[contains(., "Pros")]/following-sibling::*)[1]/li')
     for pro in pros:
