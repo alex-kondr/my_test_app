634322e372363a46953a6261f37618772ef3b43d
Oleksandr Kondratiuk <alex_kondr@outlook.com>
2025-10-30 14:25:08 +0200

    save

--- review.pluggedingolf.us/new_review.pluggedingolf.us.py
+++ review.pluggedingolf.us/new_review.pluggedingolf.us.py
@@ -4,7 +4,7 @@ import time
 import random
 
 
-OPTIONS = """--compressed -H 'User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:144.0) Gecko/20100101 Firefox/144.0' -H 'Accept: text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8' -H 'Accept-Language: uk-UA,uk;q=0.8,en-US;q=0.5,en;q=0.3' -H 'Accept-Encoding: deflate' -H 'Connection: keep-alive' -H 'Referer: https://pluggedingolf.com/' -H 'Cookie: ct_checkjs=277158b25d470642b40a35e629286e2b97386d97767df0b71258a1e48f326304; ct_ps_timestamp={time1}; ct_fkp_timestamp={time2}; ct_pointer_data=%5B%5B222%2C1308%2C707%5D%2C%5B286%2C1333%2C790%5D%2C%5B339%2C1402%2C1158%5D%2C%5B386%2C1696%2C1308%5D%2C%5B386%2C1698%2C2075%5D%2C%5B386%2C1700%2C2108%5D%2C%5B387%2C1706%2C2275%5D%2C%5B387%2C1708%2C3860%5D%2C%5B384%2C1713%2C4177%5D%2C%5B388%2C1714%2C4694%5D%2C%5B400%2C1743%2C4962%5D%2C%5B400%2C1740%2C5011%5D%2C%5B401%2C1741%2C5279%5D%2C%5B401%2C1741%2C5311%5D%2C%5B401%2C1743%2C5461%5D%2C%5B401%2C1745%2C5645%5D%2C%5B398%2C1740%2C5962%5D%2C%5B290%2C1130%2C6079%5D%2C%5B253%2C722%2C6229%5D%2C%5B239%2C591%2C6396%5D%2C%5B242%2C590%2C6897%5D%2C%5B520%2C1332%2C7014%5D%2C%5B569%2C1472%2C7163%5D%2C%5B505%2C1692%2C7314%5D%2C%5B507%2C1693%2C7848%5D%2C%5B375%2C572%2C12836%5D%2C%5B400%2C257%2C12920%5D%2C%5B421%2C206%2C13070%5D%2C%5B421%2C205%2C13387%5D%2C%5B421%2C198%2C13787%5D%2C%5B422%2C198%2C14137%5D%2C%5B426%2C200%2C14170%5D%2C%5B520%2C115%2C17008%5D%2C%5B311%2C414%2C17124%5D%2C%5B260%2C463%2C17292%5D%2C%5B228%2C546%2C17474%5D%2C%5B241%2C563%2C17608%5D%2C%5B242%2C565%2C17824%5D%2C%5B240%2C566%2C19059%5D%2C%5B286%2C580%2C19159%5D%2C%5B329%2C587%2C19309%5D%5D; ct_timezone=2; ct_screen_info=%7B%22fullWidth%22%3A1920%2C%22fullHeight%22%3A3195%2C%22visibleWidth%22%3A1920%2C%22visibleHeight%22%3A947%7D; apbct_headless=false; ct_checked_emails=0; ct_checked_emails_exist=0; ct_mouse_moved=true' -H 'Upgrade-Insecure-Requests: 1' -H 'Sec-Fetch-Dest: document' -H 'Sec-Fetch-Mode: navigate' -H 'Sec-Fetch-Site: same-origin' -H 'Sec-Fetch-User: ?1' -H 'Priority: u=0, i' -H 'Pragma: no-cache' -H 'Cache-Control: no-cache' -H 'TE: trailers'"""
+OPTIONS = """--compressed -H 'User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:144.0) Gecko/20100101 Firefox/144.0' -H 'Accept: text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8' -H 'Accept-Language: uk-UA,uk;q=0.8,en-US;q=0.5,en;q=0.3' -H 'Accept-Encoding: deflate' -H 'Connection: keep-alive' -H 'Referer: https://pluggedingolf.com/' -H 'Cookie: ct_checkjs=277158b25d470642b40a35e629286e2b97386d97767df0b71258a1e48f326304; ct_ps_timestamp={time1}; ct_fkp_timestamp={time2}; ct_pointer_data=%5B%5B295%2C1188%2C304%5D%2C%5B300%2C1188%2C321%5D%2C%5B262%2C1214%2C504%5D%2C%5B352%2C1071%2C621%5D%2C%5B484%2C827%2C771%5D%2C%5B473%2C786%2C921%5D%2C%5B422%2C690%2C1071%5D%2C%5B356%2C624%2C1222%5D%2C%5B332%2C619%2C1371%5D%2C%5B316%2C625%2C1655%5D%2C%5B316%2C627%2C1672%5D%2C%5B316%2C628%2C2322%5D%2C%5B518%2C666%2C3375%5D%2C%5B243%2C571%2C3490%5D%2C%5B232%2C571%2C3801%5D%2C%5B232%2C573%2C3874%5D%2C%5B232%2C574%2C4170%5D%2C%5B233%2C575%2C4191%5D%2C%5B242%2C584%2C5108%5D%2C%5B434%2C590%2C5242%5D%2C%5B473%2C594%2C5443%5D%2C%5B513%2C598%2C5542%5D%2C%5B513%2C597%2C6026%5D%2C%5B511%2C595%2C6710%5D%2C%5B507%2C575%2C6743%5D%2C%5B519%2C353%2C6894%5D%2C%5B531%2C353%2C7127%5D%2C%5B532%2C375%2C7997%5D%2C%5B312%2C547%2C8094%5D%2C%5B221%2C590%2C8245%5D%2C%5B256%2C559%2C8395%5D%2C%5B536%2C113%2C8545%5D%2C%5B510%2C131%2C9396%5D%2C%5B303%2C412%2C9447%5D%2C%5B238%2C534%2C9663%5D%2C%5B243%2C569%2C9747%5D%2C%5B244%2C578%2C10380%5D%2C%5B344%2C595%2C10514%5D%2C%5B421%2C596%2C10665%5D%5D; ct_timezone=2; ct_screen_info=%7B%22fullWidth%22%3A1920%2C%22fullHeight%22%3A3195%2C%22visibleWidth%22%3A1920%2C%22visibleHeight%22%3A947%7D; apbct_headless=false; ct_checked_emails=0; ct_checked_emails_exist=0; ct_mouse_moved=true; ct_has_scrolled=true; abh_tab=#abh_about' -H 'Upgrade-Insecure-Requests: 1' -H 'Sec-Fetch-Dest: document' -H 'Sec-Fetch-Mode: navigate' -H 'Sec-Fetch-Site: same-origin' -H 'Sec-Fetch-User: ?1' -H 'Priority: u=0, i' -H 'Pragma: no-cache' -H 'Cache-Control: no-cache'"""
 
 
 def run(context, session):
@@ -43,6 +43,7 @@ def process_revlist(data, context, session):
         session.do(Request(data.response_url, use='curl', force_charset='utf-8', max_age=0, options=OPTIONS.format(time1=time1, time2=time2)), process_revlist, dict(context, restart_revlist=True))
 
     elif next_url:
+        time.sleep(10)
         session.queue(Request(next_url, use='curl', force_charset='utf-8'), process_revlist, dict(context))
 
 
@@ -52,6 +53,7 @@ def process_review(data, context, session):
         time1 = int(time.time())
         time2 = time1 + random.randint(0, 50)
         session.do(Request(data.response_url, use='curl', force_charset='utf-8', max_age=0, options=OPTIONS.format(time1=time1, time2=time2)), process_review, dict(context, restart_review=True))
+        return
 
     product = Product()
     product.name = context['title'].replace(' Review', '').strip()
@@ -97,3 +99,5 @@ def process_review(data, context, session):
         product.reviews.append(review)
 
         session.emit(product)
+
+    time.sleep(10)
