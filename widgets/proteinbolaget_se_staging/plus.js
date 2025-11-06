bee55539c24711ed08ff2b7e4968adf7add7aab3
Oleksandr Kondratiuk <alex_kondr@outlook.com>
2025-11-06 13:48:20 +0200

    save

--- widgets/proteinbolaget_se_staging/plus.js
+++ widgets/proteinbolaget_se_staging/plus.js
@@ -5,3 +5,8 @@ testFreaks = window.testFreaks || [];
 testFreaks.push(["load", ["badge", "reviews"]]);
 
 (function (d, t, p) { var e = d.createElement(t); e.charset = "utf-8"; e.src = p; var s = d.getElementsByTagName(t)[0]; s.parentNode.insertBefore(e, s) })(document, "script", "https://admin.testfreaks.com/admin/widget_script/2621/test");
+
+
+var sortSVG = document.querySelector(".icon icon-xs bg-base-1")
+var sortDiv = document.querySelector(".pull-left:not(role)")
+sortDiv.appendChild(sortSVG)
