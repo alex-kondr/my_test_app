8f0875c1ec21817454989350e35f38a74cce1b40
Oleksandr Kondratiuk <alex_kondr@outlook.com>
2025-11-07 23:49:21 +0200

    save

---
+++ widgets/proteinbolaget_se_staging/old1_proteinbolaget_se_staging.js
@@ -0,0 +1,70 @@
+(function() {
+  //= require widget
+  var w = testFreaks.widget, e = testFreaks.element, div = e.curry("div"), forEach = testFreaks.forEach, t = testFreaks.translate;
+  w.setCss("%=stylesheet");
+  w.setEnableWriteReview(false);
+  w.setCssAttr("distribution-color-star", "#000");
+  w.setTranslations({ basedon: "%=t %d reviews" });
+  w.setTranslations({ verified_buyer: "%=t Verified"});
+  // w.setTranslations({ review_tab: "%=t Rating" });
+  testFreaks.options.set("showSortThreshold", 1);
+  
+  w.setEnableVerifiedBuyer(true);
+  
+  w.setOptions({ onReviews: onReviews });
+  w.setOptions({ onData: onData });
+  
+  function onData(data) {
+    console.log(data);
+  }
+  
+  function onReviews(reviews) {
+    console.log(reviews);
+    
+    var revs = document.querySelectorAll(".testfreaks .article:not(.upd)");
+    forEach(revs, function(_, r) {
+      r.classList.add('upd');
+      r.setAttribute("style", "display: flex !important;")
+      // console.log('r=', r);
+      var logo = r.querySelector(".text-logo");
+      logo.setAttribute("style", "display: none !important");
+
+      var footer = r.querySelector(".footer")
+      footer.setAttribute("style", "display: none !important");
+      
+      var time = r.querySelector("time");
+      time.setAttribute("style", "display: none !important");
+      
+      var divHrows = r.querySelectorAll(".hrow");
+      forEach(divHrows, function(_, divHrow) {
+        divHrow.setAttribute("style", "margin-left: 0px !important; display: flex !important;");
+        
+        var span = divHrow.querySelector("span");
+        if (span) {
+          span.setAttribute("style", "display: none !important");
+        };
+      });
+      
+      var divHrow = r.querySelector(".hrow");
+      var parent = r.querySelector(".header");
+      parent.appendChild(divHrow)
+      
+      var score = reviews["reviews"][_]["score"];
+      var divHrow = r.querySelector(".hrow");
+      var divScore = div("tf-grade", { text: score + " av 5" });
+      divScore.setAttribute("style", "margin: .3em !important;");
+      divHrow.appendChild(divScore);
+      
+      var content = r.querySelector(".content");
+      content.setAttribute("style", "margin-left: 0px !important");
+      
+    });
+  };
+
+  testFreaks.options.set("showTabCounts", false);
+
+  w.setApiClientName("oriflame-se-sv");
+  w.setProductId("42495");
+  
+  w.run();
+})();
