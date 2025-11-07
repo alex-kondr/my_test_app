8de74d5880539786d863ba1bd9413896d73c0717
Oleksandr Kondratiuk <alex_kondr@outlook.com>
2025-11-07 15:43:51 +0200

    save

--- widgets/proteinbolaget_se_staging/plus.js
+++ widgets/proteinbolaget_se_staging/plus.js
@@ -6,7 +6,20 @@ testFreaks.push(["load", ["badge", "reviews"]]);
 
 (function (d, t, p) { var e = d.createElement(t); e.charset = "utf-8"; e.src = p; var s = d.getElementsByTagName(t)[0]; s.parentNode.insertBefore(e, s) })(document, "script", "https://admin.testfreaks.com/admin/widget_script/2621/test");
 
+// (function (d, t, p) { var e = d.createElement(t); e.charset = "utf-8"; e.src = p; var s = d.getElementsByTagName(t)[0]; s.parentNode.insertBefore(e, s) })(document, "script", "https://js.testfreaks.com/onpage/oriflame-se-sv/reviews.json?key=46989");
 
-var sortSVG = document.querySelector(".icon icon-xs bg-base-1")
-var sortDiv = document.querySelector(".pull-left:not(role)")
-sortDiv.appendChild(sortSVG)
+
+// var sortSVG = document.querySelector(".icon icon-xs bg-base-1")
+// var sortDiv = document.querySelector(".pull-left:not(role)")
+// sortDiv.appendChild(sortSVG)
+
+// var score = reviews[0]reviews[_]['score'];
+// var divScore = div("tf-grade", { text: score + " av 5" })
+// var divScoreByDoc = r.querySelector(".score .hcol")
+// console.log(divScoreByDoc)
+// divScoreByDoc.appendChild(divScore)
+
+var r = document.querySelector("hrow");
+var divHrow = r.querySelector(".hrow");
+var parent = r.querySelector(".header");
+parent.appendChild(divHrow)
