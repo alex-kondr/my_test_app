e548a8021edf42bc738b3ade4280b3ed0d6ccd74
Oleksandr Kondratiuk <alex_kondr@outlook.com>
2026-02-10 17:07:12 +0200

    save

--- review.carseatblog.us/new_review.carseatblog.us.py
+++ review.carseatblog.us/new_review.carseatblog.us.py
@@ -2,8 +2,11 @@ from agent import *
 from models.products import *
 
 
+XTITLE = ['Best ', 'Recommended ', 'THANK YOU!', 'About', 'Privacy Policy', 'KIM or Bust!']
+
+
 def run(context, session):
-    session.queue(Request('https://www.iculture.nl/reviews/', use='curl', force_charset='utf-8'), process_revlist, dict())
+    session.queue(Request('https://www.iculture.nl/reviews/'), process_revlist, dict())
 
 
 def process_revlist(data, context, session):
@@ -11,18 +14,20 @@ def process_revlist(data, context, session):
     for rev in revs:
         title = rev.xpath('text()').string()
         url = rev.xpath('@href').string()
-        session.queue(Request(url, use='curl', force_charset='utf-8'), process_review, dict(title=title, url=url))
+
+        if 'review' in title.lower() and not any(title.startswith(xtitle) for xtitle in XTITLE):
+            session.queue(Request(url), process_review, dict(title=title, url=url))
 
     next_url = data.xpath('//a[contains(@class, "next")]/@href').string()
     if next_url:
-        session.queue(Request(next_url, use='curl', force_charset='utf-8'), process_revlist, dict())
+        session.queue(Request(next_url), process_revlist, dict())
 
 
 def process_review(data, context, session):
     product = Product()
-    product.name = context['title'].replace('', '').strip()
+    product.name = context['title'].split(' Review: ')[0].strip()
     product.url = context['url']
-    product.ssid = product.url.split('/')[-2]
+    product.ssid = product.url.split('/')[-3]
     product.category = 'Tech'
 
     review = Review()
@@ -31,23 +36,23 @@ def process_review(data, context, session):
     review.url = product.url
     review.ssid = product.ssid
 
-    date = data.xpath('//meta[@property="article:published_time"]/@content|//time/@datetime').string()
+    date = data.xpath('//meta[@property="article:published_time"]/@content').string()
     if date:
         review.date = date.split('T')[0]
 
-    author = data.xpath('/text()').string()
-    author_url = data.xpath('/@href').string()
+    author = data.xpath('//span[contains(@class, "author")]//text()').string(multiple=True)
+    author_url = data.xpath('//span[contains(@class, "author")]//a/@href').string()
     if author and author_url:
-        author_ssid = author_url.split('/')[-1]
+        author_ssid = author_url.split('/')[-2]
         review.authors.append(Person(name=author, ssid=author_ssid, profile_url=author_url))
     elif author:
         review.authors.append(Person(name=author, ssid=author))
 
-    grade_overall = data.xpath('//text()').string()
-    if grade_overall:
-        review.grades.append(Grade(type='overall', value=float(grade_overall), best=))
+    # grade_overall = data.xpath('//text()').string()
+    # if grade_overall:
+    #     review.grades.append(Grade(type='overall', value=float(grade_overall), best=))
 
-    pros = data.xpath('(//h3[contains(., "Pros")]/following-sibling::*)[1]/li')
+    pros = data.xpath('//h4[contains(., "ADVANTAGES:")]/following-sibling::ul/li')
     for pro in pros:
         pro = pro.xpath('.//text()').string(multiple=True)
         if pro:
@@ -55,7 +60,7 @@ def process_review(data, context, session):
             if len(pro) > 1:
                 review.add_property(type='pros', value=pro)
 
-    cons = data.xpath('(//h3[contains(., "Cons")]/following-sibling::*)[1]/li')
+    cons = data.xpath('//h4[contains(., "DISADVANTAGES:")]/following-sibling::ul/li')
     for con in cons:
         con = con.xpath('.//text()').string(multiple=True)
         if con:
@@ -63,17 +68,16 @@ def process_review(data, context, session):
             if len(con) > 1:
                 review.add_property(type='cons', value=con)
 
-    summary = data.xpath('//div[h3[contains(text(), "Summary")]]/div//text()').string(multiple=True)
-    if summary:
-        review.add_property(type='summary', value=summary)
+    conclusion = data.xpath('//h4[contains(., "THE BOTTOM LINE")]/following-sibling::p//text()').string(multiple=True)
+    if not conclusion:
+        data.xpath('//p[strong[contains(., "Summary")]]/following-sibling::p[not(regexp:test(., "Pros:|Cons:"))]//text()').sring(multiple=True)
 
-    conclusion = data.xpath('//h3[contains(., "Conclusion")]/following-sibling::p//text()').string(multiple=True)
     if conclusion:
         review.add_property(type='conclusion', value=conclusion)
 
-    excerpt = data.xpath('//h3[contains(., "Conclusion")]/preceding-sibling::p//text()').string(multiple=True)
+    excerpt = data.xpath('//h4[contains(., "THE BOTTOM LINE")]/preceding-sibling::p[not((preceding::h4|preceding::p/span|preceding::p/strong)[regexp:test(., "ADVANTAGES|DISADVANTAGES|THE BOTTOM LINE|Pros:|Cons:|Summary")])]//text()[not(regexp:test(., "Pros:|Cons:|Summary"))]').string(multiple=True)
     if not excerpt:
-        excerpt = data.xpath('//text()').string(multiple=True)
+        excerpt = data.xpath('//div[contains(@class, "content")]/p[not((preceding::h4|preceding::p/span|preceding::p/strong)[regexp:test(., "ADVANTAGES|DISADVANTAGES|THE BOTTOM LINE|Pros:|Cons:|Summary")])]//text()[not(regexp:test(., "Pros:|Cons:|Summary"))]').string(multiple=True)
 
     if excerpt:
         review.add_property(type='excerpt', value=excerpt)
