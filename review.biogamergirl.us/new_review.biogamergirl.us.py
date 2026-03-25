0f29de343f4cbb7ce6b810ac5811fddd3fb71e81
Oleksandr Kondratiuk <alex_kondr@outlook.com>
2026-03-25 16:04:13 +0200

    save

--- review.biogamergirl.us/new_review.biogamergirl.us.py
+++ review.biogamergirl.us/new_review.biogamergirl.us.py
@@ -1,9 +1,10 @@
 from agent import *
 from models.products import *
 import re
+import time
 
 
-CONCLUSION_WORDS = ['Conclusion', 'In the end, ', 'In conclusion, ', 'Overall, ', 'In summary, ']
+SLEEP = 2
 
 
 def run(context, session):
@@ -11,6 +12,8 @@ def run(context, session):
 
 
 def process_revlist(data, context, session):
+    time.sleep(SLEEP)
+
     revs = data.xpath('//h3[contains(@class, "post-title")]/a')
     for rev in revs:
         title = rev.xpath('.//text()').string()
@@ -23,16 +26,32 @@ def process_revlist(data, context, session):
 
 
 def process_review(data, context, session):
+    time.sleep(SLEEP)
+
     product = Product()
     product.name = context['title'].split('Review: ')[-1].split(':')[0].split(' Review ')[0].split(' (')[0].replace(' Review', '').replace('Review -', '').replace('Oh...Sir!', '').strip()
     product.url = context['url']
-    product.ssid = data.xpath('//div[contains(@id, "post-body")]/@id').string().split('-')[-1]
     product.category = 'Tech'
-    product.manufacturer = data.xpath('//strong[contains(text(), "Developer:")]/following-sibling::text()[1]').string()
 
-    platforme = data.xpath('//strong[contains(text(), "Reviewed on:")]/following-sibling::text()[1]').string()
+    ssid = data.xpath('//div[contains(@id, "post-body")]/@id').string()
+    if ssid:
+        product.ssid = ssid.split('-')[-1]
+    else:
+        product.ssid = product.url.split('/')[-1].replace('.html', '').replace('review-', '')
+
+    manufacturer = data.xpath('//strong[contains(text(), "Developer:")]/following-sibling::text()').string()
+    if not manufacturer:
+        manufacturer = data.xpath('//text()[contains(., "Developer:")]').string()
+
+    if manufacturer:
+        product.manufacturer = manufacturer.split('Developer:')[-1].split('|')[0].strip()
+
+    platforme = data.xpath('//strong[contains(text(), "Reviewed on:")]/following-sibling::text()').string()
+    if not platforme:
+        platforme = data.xpath('//text()[contains(., "Reviewed on:")]').string()
+
     if platforme:
-        product.category = 'Games|' + platforme
+        product.category = 'Games|' + platforme.split('Reviewed on:')[-1].split('|')[0].strip()
 
     review = Review()
     review.type = 'pro'
@@ -51,8 +70,11 @@ def process_review(data, context, session):
 
     if not grade_overall:
         grade_overall = data.xpath('//strong[contains(text(), "Score:")]/text()').string()
+        if not grade_overall:
+            grade_overall = data.xpath('//strong[contains(text(), "SCORE:")]/text()').string()
+
         if grade_overall:
-            grade_overall = grade_overall.replace('Score:', '').split('/')[0]
+            grade_overall = grade_overall.replace('Score:', '').replace('SCORE:', '').split('/')[0].split('out')[0].replace('Review', '')
             if grade_overall and float(grade_overall) > 0:
                 review.grades.append(Grade(type='overall', value=float(grade_overall), best=10.0))
 
@@ -77,6 +99,8 @@ def process_review(data, context, session):
     conclusion = data.xpath('//u[contains(., "Conclusion")]/following-sibling::text()').string(multiple=True)
     if not conclusion:
         conclusion = data.xpath('//p[preceding-sibling::h2[1][contains(., "Verdict")]]//text()').string(multiple=True)
+    if not conclusion:
+        conclusion = data.xpath('//text()[contains(., "In conclusion, ")]/following-sibling::text()|(//text()[contains(., "In conclusion, ")]/following-sibling::a|//text()[contains(., "In conclusion, ")]/following-sibling::b|//text()[contains(., "In conclusion, ")]/following-sibling::i|//text()[contains(., "In conclusion, ")]/following-sibling::em|//text()[contains(., "In conclusion, ")]/following-sibling::strong)/text()').string(multiple=True)
 
     if conclusion:
         if summary:
@@ -89,7 +113,15 @@ def process_review(data, context, session):
 
     excerpt = data.xpath('//h2[contains(., "Verdict")]/preceding-sibling::p[not(contains(., "Strengths:") or contains(., "Weaknesses:"))]//text()').string(multiple=True)
     if not excerpt:
+        excerpt = data.xpath('//p[contains(., "In conclusion, ")]/preceding-sibling::p//text()').string(multiple=True)
+    if not excerpt:
+        excerpt = data.xpath('//text()[contains(., "In conclusion, ")]/preceding-sibling::text()|(//text()[contains(., "In conclusion, ")]/preceding-sibling::a|//text()[contains(., "In conclusion, ")]/preceding-sibling::b|//text()[contains(., "In conclusion, ")]/preceding-sibling::i|strong)/text()').string(multiple=True)
+    if not excerpt:
         excerpt = data.xpath('//div[contains(@id, "post-body")]/p[not(regexp:test(., "Strengths:|Weaknesses:|Developer:|Publisher:|Score:") or preceding::p[regexp:test(., "Developer:|Publisher:")])]//text()').string(multiple=True)
+    if not excerpt:
+        excerpt = data.xpath('//div[contains(@id, "post-body")]/div/p[not(regexp:test(., "Strengths:|Weaknesses:|Developer:|Publisher:|Score:|SCORE:") or preceding::p[regexp:test(., "Developer:|Publisher:")])]//text()').string(multiple=True)
+    if not excerpt:
+        excerpt = data.xpath('(//div[contains(@id, "post-body")]/a|//div[contains(@id, "post-body")]/b|//div[contains(@id, "post-body")]/i|//div[contains(@id, "post-body")]/strong|//div[contains(@id, "post-body")])/text()').string(multiple=True)
 
     if excerpt:
         review.add_property(type='excerpt', value=excerpt)
