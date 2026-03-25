f0be5f87c831ac55cb47ed082426b4ab55fb73bf
Oleksandr Kondratiuk <alex_kondr@outlook.com>
2026-03-25 13:32:02 +0200

    save

--- review.biogamergirl.us/new_review.biogamergirl.us.py
+++ review.biogamergirl.us/new_review.biogamergirl.us.py
@@ -7,7 +7,7 @@ CONCLUSION_WORDS = ['Conclusion', 'In the end, ', 'In conclusion, ', 'Overall, '
 
 
 def run(context, session):
-    session.queue(Request('http://www.biogamergirl.com/search/label/Video%20Game%20Reviews'), process_revlist, dict())
+    session.queue(Request('http://www.biogamergirl.com/search/label/Video%20Game%20Reviews', max_age=0), process_revlist, dict())
 
 
 def process_revlist(data, context, session):
@@ -15,29 +15,24 @@ def process_revlist(data, context, session):
     for rev in revs:
         title = rev.xpath('.//text()').string()
         url = rev.xpath('@href').string()
-        session.queue(Request(url), process_review, dict(title=title, url=url))
+        session.queue(Request(url, max_age=0), process_review, dict(title=title, url=url))
 
     next_url = data.xpath('//a[@title="More posts"]/@href').string()
     if next_url:
-        session.queue(Request(next_url), process_revlist, dict())
+        session.queue(Request(next_url, max_age=0), process_revlist, dict())
 
 
 def process_review(data, context, session):
-    excerpt = data.xpath('//div[contains(@id, "post-body")]//text()').string(multiple=True)
-    if not excerpt:
-        return
-
     product = Product()
     product.name = context['title'].split('Review: ')[-1].split(':')[0].split(' Review ')[0].split(' (')[0].replace(' Review', '').replace('Review -', '').replace('Oh...Sir!', '').strip()
     product.url = context['url']
     product.ssid = data.xpath('//div[contains(@id, "post-body")]/@id').string().split('-')[-1]
     product.category = 'Tech'
+    product.manufacturer = data.xpath('//strong[contains(text(), "Developer:")]/following-sibling::text()[1]').string()
 
-    if " (" in context['title']:
-        product.category = 'Games|' + context['title'].split(' (')[-1].replace('Review', '').strip(' ()')
-
-    if 'Developer:' in excerpt:
-        product.manufacturer = excerpt.split('Developer:')[-1].split('Publisher:')[0].strip()
+    platforme = data.xpath('//strong[contains(text(), "Reviewed on:")]/following-sibling::text()[1]').string()
+    if platforme:
+        product.category = 'Games|' + platforme
 
     review = Review()
     review.type = 'pro'
@@ -54,25 +49,51 @@ def process_review(data, context, session):
             grade_best = float(grade_best.group().strip(' /'))
             review.grades.append(Grade(type='overall', value=grade_val, best=grade_best))
 
+    if not grade_overall:
+        grade_overall = data.xpath('//strong[contains(text(), "Score:")]/text()').string()
+        if grade_overall:
+            grade_overall = grade_overall.replace('Score:', '').split('/')[0]
+            if grade_overall and float(grade_overall) > 0:
+                review.grades.append(Grade(type='overall', value=float(grade_overall), best=10.0))
+
+    pros = data.xpath('//p[contains(strong, "Strengths:")]/text()[normalize-space(.)]')
+    for pro in pros:
+        pro = pro.string(multiple=True)
+        if pro:
+            pro = pro.strip(' +-*.:;•,–')
+            if len(pro) > 1:
+                review.add_property(type='pros', value=pro)
+
+    cons = data.xpath('//p[contains(strong, "Weaknesses:")]/text()[normalize-space(.)]')
+    for con in cons:
+        con = con.string(multiple=True)
+        if con:
+            con = con.strip(' +-*.:;•,–')
+            if len(con) > 1:
+                review.add_property(type='cons', value=con)
+
+    summary = data.xpath('//p[contains(strong, "BioGamer Girl Review Verdict")]/following-sibling::p[1]//text()').string(multiple=True)
+
     conclusion = data.xpath('//u[contains(., "Conclusion")]/following-sibling::text()').string(multiple=True)
+    if not conclusion:
+        conclusion = data.xpath('//p[preceding-sibling::h2[1][contains(., "Verdict")]]//text()').string(multiple=True)
+
     if conclusion:
+        if summary:
+            review.add_property(type='summary', value=summary)
+
         review.add_property(type='conclusion', value=conclusion)
 
-        excerpt = excerpt.replace(conclusion, '').strip()
+    elif summary:
+        review.add_property(type='conclusion', value=summary)
 
-    for word in CONCLUSION_WORDS:
-        if word in excerpt:
-            excerpt, conclusion_2 = excerpt.rsplit(word, 1)
+    excerpt = data.xpath('//h2[contains(., "Verdict")]/preceding-sibling::p[not(contains(., "Strengths:") or contains(., "Weaknesses:"))]//text()').string(multiple=True)
+    if not excerpt:
+        excerpt = data.xpath('//div[contains(@id, "post-body")]/p[not(regexp:test(., "Strengths:|Weaknesses:|Developer:|Publisher:|Score:") or preceding::p[regexp:test(., "Developer:|Publisher:")])]//text()').string(multiple=True)
 
-            if not conclusion:
-                conclusion_2 = conclusion_2.split('Features:')[0].split('Game Information:')[0].split('Developer:')[0].split('Publisher:')[0].split('Score:')[0].strip()
-                review.add_property(type='conclusion', value=conclusion_2)
+    if excerpt:
+        review.add_property(type='excerpt', value=excerpt)
 
-            break
+        product.reviews.append(review)
 
-    excerpt = excerpt.split('Features:')[0].split('Game Information:')[0].split('Developer:')[0].split('Publisher:')[0].split('Score:')[0].strip()
-    review.add_property(type='excerpt', value=excerpt)
-
-    product.reviews.append(review)
-
-    session.emit(product)
+        session.emit(product)
