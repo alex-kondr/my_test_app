45fabb05c8c31ba5784d1d7c05694924c257875b
Oleksandr Kondratiuk <alex_kondr@outlook.com>
2026-04-15 13:39:24 +0300

    save

--- review.sega-mag.fr (BB)/test.py
+++ review.sega-mag.fr (BB)/test.py
@@ -12,12 +12,11 @@ from product_test.test_logs import LogProduct, TestLogProduct
 import product_test.list_of_agents as agents
 
 
-agent = agents.WARDOW_COM_US
+agent = agents.SEGA_MAG_FR
 # agent = agents.TEST
 reload = 1
 session_id = 0
 
-# excerpt: 1
 
 if __name__ == "__main__":
     product = Product(agent, reload=reload, session_id=session_id)
--- review.wardow.com/new_review.wardow.com.py
+++ review.wardow.com/new_review.wardow.com.py
@@ -6,6 +6,30 @@ import simplejson
 XCAT = ['New', 'Brands', 'Sale %', 'Inspiration']
 
 
+def remove_emoji(string):
+    emoji_pattern = re.compile("["
+                               u"\U0001F600-\U0001F64F"  # emoticons
+                               u"\U0001F300-\U0001F5FF"  # symbols & pictographs
+                               u"\U0001F680-\U0001F6FF"  # transport & map symbols
+                               u"\U0001F1E0-\U0001F1FF"  # flags (iOS)
+                               u"\U00002500-\U00002BEF"  # chinese char
+                               u"\U00002702-\U000027B0"
+                               u"\U00002702-\U000027B0"
+                               u"\U000024C2-\U0001F251"
+                               u"\U0001f926-\U0001f937"
+                               u"\U00010000-\U0010ffff"
+                               u"\u2640-\u2642"
+                               u"\u2600-\u2B55"
+                               u"\u200d"
+                               u"\u23cf"
+                               u"\u23e9"
+                               u"\u231a"
+                               u"\ufe0f"  # dingbats
+                               u"\u3030"
+                               "]+", flags=re.UNICODE)
+    return emoji_pattern.sub(r'', string)
+
+
 def run(context, session):
     session.sessionbreakers = [SessionBreak(max_requests=10000)]
     session.queue(Request('https://www.wardow.com/en/', max_age=0), process_frontpage, dict())
@@ -68,7 +92,7 @@ def process_reviews(data, context, session):
 
     revs = revs_json.get('reviews', [])
     for rev in revs:
-        if rev.get('language') != 'en' or rev.get('syndicationData'):
+        if rev.get('language') != 'en' or rev.get('syndicationData') or rev.get('groupingData'):
             continue
 
         review = Review()
@@ -105,15 +129,18 @@ def process_reviews(data, context, session):
 
         title = rev.get('title')
         excerpt = rev.get('content')
-        if excerpt:
-            review.title = title
+        if excerpt and len(remove_emoji(excerpt).replace('\n', '').strip()) > 2:
+            if title:
+                review.title = remove_emoji(title).replace('\n', '').strip()
         else:
             excerpt = title
 
         if excerpt:
-            review.add_property(type='excerpt', value=excerpt)
+            excerpt = remove_emoji(excerpt).replace('\n', '').strip()
+            if len(excerpt) > 2:
+                review.add_property(type='excerpt', value=excerpt)
 
-            product.reviews.append(review)
+                product.reviews.append(review)
 
     revs_cnt = revs_json.get('pagination', {}).get('total', 0)
     offset = context.get('offset', 0) + 5
