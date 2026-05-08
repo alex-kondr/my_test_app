9fccdff9f93842db2040db924af20b3e650b83c8
Oleksandr Kondratiuk <alex_kondr@outlook.com>
2026-05-08 10:05:15 +0300

    save

--- review.ichip.ru/test.py
+++ review.ichip.ru/test.py
@@ -12,14 +12,11 @@ from product_test.test_logs import LogProduct, TestLogProduct
 import product_test.list_of_agents as agents
 
 
-agent = agents.VG_RELOADED_US
+agent = agents.ICHIP_RU
 # agent = agents.TEST
 reload = 1
-session_id = 6844391
+session_id = 0
 
-# name: 9
-# conclusion: 3
-# excerpt: 5
 
 if __name__ == "__main__":
     product = Product(agent, reload=reload, session_id=session_id)
