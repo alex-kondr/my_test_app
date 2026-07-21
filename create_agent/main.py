96fc043e63bf922bfeefdcc5e3b6655f0100c2e5
Oleksandr Kondratiuk <alex_kondr@outlook.com>
2026-07-21 23:00:32 +0300

    save

--- create_agent/main.py
+++ create_agent/main.py
@@ -12,7 +12,7 @@ from create_agent.agent import AgentForm, ProcessRun, TypeAgent
 
 agent = AgentForm(
     # name="reviews.fotokoch.de",
-    agent_id="19802",
+    agent_id="19321",
     new_agent=False
     )
 agent.create_run(
---
+++ review.soccercleats101.us/upload_code.py
@@ -0,0 +1,28 @@
+import sys
+import os
+
+
+current = os.path.dirname(os.path.realpath(__file__))
+parent = os.path.dirname(current)
+sys.path.append(parent)
+
+
+from product_test.functions import upload_code
+import product_test.list_of_agents as agents
+
+
+agent = agents.SOCCERCLEATS101_COM
+
+
+with open("review.soccercleats101.us/new_review.soccercleats101.us.py", "r", encoding="utf-8") as file:
+    agent_code = file.read()
+
+agent_code = agent_code.replace(
+            "(data: Response, context: dict[str, str], session: Session)",
+            "(data, context, session)"
+        ).replace(
+            "(context: dict[str, str], session: Session)",
+            "(context, session)"
+        )
+
+upload_code(agent, agent_code, run=True)
