be31073deb95c390d0ff48c4f5c2727db7f6c9bc
Oleksandr Kondratiuk <alex_kondr@outlook.com>
2026-08-22 18:53:10 +0300

    save

--- reviews.weleda.de/new_reviews.weleda.de.py
+++ reviews.weleda.de/new_reviews.weleda.de.py
@@ -1,6 +1,31 @@
 from agent import *
 from models.products import *
 import simplejson
+import re
+
+
+def remove_emoji(string):
+    emoji_pattern = re.compile("["
+                               u"\U0001F600-\U0001F64F"  # emoticons
+                               u"\U0001F300-\U0001F5FF"  # symbols & pictographs
+                               u"\U0001F680-\U0001F6FF"  # transport & map symbols
+                               u"\U0001F1E0-\U0001F1FF"  # flags (iOS)
+                               u"\U00002500-\U00002BEF"  # chinese char
+                               u"\U00002702-\U000027B0"
+                               u"\U00002702-\U000027B0"
+                               u"\U000024C2-\U0001F251"
+                               u"\U0001f926-\U0001f937"
+                               u"\U00010000-\U0010ffff"
+                               u"\u2640-\u2642"
+                               u"\u2600-\u2B55"
+                               u"\u200d"
+                               u"\u23cf"
+                               u"\u23e9"
+                               u"\u231a"
+                               u"\ufe0f"  # dingbats
+                               u"\u3030"
+                               "]+", flags=re.UNICODE)
+    return emoji_pattern.sub(r'', string)
 
 
 def strip_namespace(data):
@@ -117,13 +142,14 @@ def process_reviews(data: Response, context: dict[str, str], session: Session):
 
         title = rev.get('Title')
         excerpt = rev.get('ReviewText')
-        if excerpt and len(excerpt.replace('\n', '').replace('\r', '').replace('\t', '').strip()) > 2:
-            review.title = title
+        if excerpt and len(remove_emoji(excerpt).replace('\n', '').replace('\r', '').replace('\t', '').strip()) > 2:
+            if title:
+                review.title = remove_emoji(title).strip()
         else:
             excerpt = title
 
         if excerpt and '(Ursprünglich erschienen auf influenster.com)' not in excerpt:
-            excerpt = excerpt.replace('\n', '').replace('\r', '').replace('\t', '').strip()
+            excerpt = remove_emoji(excerpt).replace('\n', '').replace('\r', '').replace('\t', '').strip()
             if len(excerpt) > 2:
                 review.add_property(type='excerpt', value=excerpt)
 
